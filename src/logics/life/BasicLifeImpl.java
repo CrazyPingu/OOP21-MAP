78b836a4ea3caa844315a0c31dfe365a53e11f9a
ElektroDuck <luca.babboni01@gmail.com>
2022-03-31 19:45:54 +0200

    create class BasicLifeImpl

---
+++ src/logics/life/BasicLifeImpl.java
@@ -0,0 +1,34 @@
+package logics.life;
+
+public class BasicLifeImpl implements BasicLife {
+
+	private int currentHealth; 
+	private int maxHealth;
+	
+	public BasicLifeImpl(int health) {
+		this.currentHealth = health; 
+		this.maxHealth = health; 
+	}
+	
+	
+	@Override
+	public void takeDamage(int damageValue) {	
+		this.currentHealth = (this.currentHealth - damageValue) < 0 ? 0 : this.currentHealth - damageValue;  
+	}
+
+	@Override
+	public void heal(int healValue) {
+		this.currentHealth = (this.currentHealth + healValue) > maxHealth ? maxHealth : this.currentHealth + healValue; 
+	}
+
+	@Override
+	public int getCurrentHealth() {
+		return this.currentHealth;
+	}
+
+	@Override
+	public int getMaxHealth() {
+		return this.maxHealth;
+	}
+
+}
