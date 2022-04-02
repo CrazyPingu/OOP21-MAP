92ce351fb5310945fe4abe431450c6646e31f593
ElektroDuck <luca.babboni01@gmail.com>
2022-04-02 16:45:31 +0200

    implemented artefactFactory

--- src/logics/artefact/ArtefactFactory.java
+++ src/logics/artefact/ArtefactFactory.java
@@ -5,10 +5,21 @@ package logics.artefact;
  * Interface that declares methods for the artefact factory.
  *
  */
+
 public interface ArtefactFactory {
 	
 	/**
-	 * @return a new artefact.
+	 * @return a new artefact that encrease the maxHealth of 1 unit.
 	 */
-	public Artefact createArtefact();
+	public Artefact createMaxHealthIncreaser();
+	
+	/**
+	 * @return a new artefact that encrease the damage of 1 unit.
+	 */
+	public Artefact createDamageIncreaser();
+	
+	/**
+	 * @return a new artefact that encrease the number action of 1 unit.
+	 */
+	public Artefact createActionIncreaser();
 }
