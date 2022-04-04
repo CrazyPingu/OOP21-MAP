dadc532485672638414bd012a4bc24f711a47edf
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-04 17:28:41 +0200

    Created the Constant class

--- src/utilis/Constant.java
+++ src/utilis/Constant.java
@@ -1,5 +1,31 @@
 package utilis;
 
+import java.awt.Toolkit;
+
+/**
+ * 
+ * Class that will contain all constant variable and method
+ *
+ */
 public class Constant {
+
+	public static final int WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+	public static final int HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+	
+	/**
+	 * @param value : the number of pixel that we want the ratio
+	 * @return the number of pixel that matches to the same in 1920
+	 */
+	public static final int horizontalAspectRatio(int value) {
+		return HEIGHT * value / 1920;
+	}
+	
+	/**
+	 * @param value : the number of pixel that we want the ratio
+	 * @return the number of pixel that matches to the same in 1080
+	 */
+	public static final int verticalAspectRatio(int value) {
+		return HEIGHT * value / 1080;
+	}
 
 }
