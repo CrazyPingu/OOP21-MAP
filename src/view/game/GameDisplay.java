14f2d673f52646095f9f2437ef97f2327b84c483
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-04 20:51:15 +0200

    Added JavaDoc for GameDisplay

--- src/view/game/GameDisplay.java
+++ src/view/game/GameDisplay.java
@@ -4,11 +4,19 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JPanel;
 
-
-public class GameDisplay extends JPanel{
-
+/**
+ * 
+ * JPanel that will contain the top area of the game (with the stats, the log
+ * and the gaming field) and the area for the action of the player
+ *
+ */
+public class GameDisplay extends JPanel {
 
 	private static final long serialVersionUID = 5038270668520802248L;
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public GameDisplay() {
 		this.setLayout(new BorderLayout());
 		this.setBackground(Color.black);
@@ -16,7 +24,7 @@ public class GameDisplay extends JPanel{
 		top.setOpaque(false);
 		JPanel action = new JPanel();
 		action.setOpaque(false);
-		
+
 		this.add(top, BorderLayout.NORTH);
 		this.add(action, BorderLayout.SOUTH);
 	}
