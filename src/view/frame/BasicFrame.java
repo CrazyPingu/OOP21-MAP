35db37a2df36455f2ffa0cf58be00745faf25832
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-05 15:59:14 +0200

    Renamed class from basicFrame to BasicFrame

--- src/view/frame/BasicFrame.java
+++ src/view/frame/BasicFrame.java
@@ -11,7 +11,7 @@ import javax.swing.JPanel;
  * Class to create the frame that will contain the whole GUI using the CardLayout
  * 
  */
-public class basicFrame extends JFrame {
+public class BasicFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	static GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
@@ -21,7 +21,7 @@ public class basicFrame extends JFrame {
 	/**
 	 * {@inheritDoc}
 	 */
-	public basicFrame() {
+	public BasicFrame() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setUndecorated(true);
 		device.setFullScreenWindow(this);
