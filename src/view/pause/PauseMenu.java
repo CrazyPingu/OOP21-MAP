296c0510b95b4f172223cd390e14718f65d91e77
Barto00 <federico.bartolini6@studio.unibo.it>
2022-04-07 11:53:16 +0200

    fix Pause Menu and add javadoc

--- src/view/pause/PauseMenu.java
+++ src/view/pause/PauseMenu.java
@@ -14,11 +14,22 @@ import view.pause.action.NewGameAction;
 import view.pause.action.QuitAction;
 import view.pause.action.ResumeAction;
 
-public class PauseMenu extends JPanel{
+/**
+ * 
+ * PauseMenu is a class that is used to implement the pause menu himself. Here
+ * we can find four buttons: Resume, New Game, Main Menu and Quit.
+ *
+ */
+
+public class PauseMenu extends JPanel {
 
     private static final long serialVersionUID = 8006565750742104587L;
     Image image;
 
+    /**
+     * 
+     * @param frame : the frame passed in input, change the screen using buttons
+     */
     public PauseMenu(BasicFrame frame) {
         try {
             image = ImageIO.read(getClass().getResource("/resources/pause/Background.jpg"));
@@ -42,10 +53,14 @@ public class PauseMenu extends JPanel{
         newgame.addActionListener(new NewGameAction(frame));
         mainmenu.addActionListener(new MainMenuAction(frame));
         quit.addActionListener(new QuitAction());
-        
 
     }
 
+    /**
+     * 
+     * @param name : the string that button will contain
+     * @return the button created
+     */
     private JButton createJB(String name) {
         JButton temp = new JButton(name);
         temp.setFont(Constant.genericFont("Tahoma", Font.PLAIN, Constant.horizontalAspectRatio(40)));
@@ -56,6 +71,11 @@ public class PauseMenu extends JPanel{
         return temp;
     }
 
+    /**
+     * 
+     * @param name : the string that label will show
+     * @return the label with string passed
+     */
     private JLabel labeltext(String name) {
         JLabel pause = new JLabel(name, SwingConstants.CENTER);
         pause.setForeground(Color.white);
@@ -63,7 +83,7 @@ public class PauseMenu extends JPanel{
         pause.setFont(Constant.genericFont("Arial", Font.PLAIN, Constant.horizontalAspectRatio(80)));
         return pause;
     }
-
+        
     private GridBagConstraints setButtonPosition(int gridy, int topPad, int botPad) {
         GridBagConstraints a = new GridBagConstraints();
         topPad = Constant.verticalAspectRatio(topPad);
@@ -73,6 +93,9 @@ public class PauseMenu extends JPanel{
         return a;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(ImageModifier.scaleFullScreen(image), 0, 0, null);
