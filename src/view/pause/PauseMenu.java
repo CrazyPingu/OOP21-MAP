e9fa454271d5eca23826c416d5d5ad7016b15b51
Barto00 <federico.bartolini6@studio.unibo.it>
2022-04-07 11:36:10 +0200

    add pause background and fixed pause menu

--- src/view/pause/PauseMenu.java
+++ src/view/pause/PauseMenu.java
@@ -1,66 +1,72 @@
 package view.pause;
 
 import java.awt.*;
-import java.awt.event.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import utilis.Constant;
-import view.pause.action.MainMenuAction;
-import view.pause.action.NewGameAction;
-import view.pause.action.QuitAction;
-import view.pause.action.ResumeAction;
+import utilis.ImageModifier;
 
-public class PauseMenu extends JPanel implements ActionListener{
+public class PauseMenu extends JPanel implements ActionListener {
 
-    public static final int WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-    public static final int HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
     private static final long serialVersionUID = 8006565750742104587L;
-
+    Image image;
     /*
-     * Sarà composto da 4 bottoni: Continue, New Game, Main Menu, Quit
-     * Continue : Mostra la CardLayout del Gioco e chiude quella del menu di pausa
-     * New Game : Mostra la CardLayout del Gioco ma dall'inizio, perdendo tutti i salvataggi fatti fino a quel punto
-     * Main Menu : Mostrerà il CardLayout del Menù Principale (tutti i salvataggi verranno persi anche in questo caso)
-     * Quit : Chiuderà il gioco (System.Exit(0);)
+     * Sarà composto da 4 bottoni: Continue, New Game, Main Menu, Quit Continue :
+     * Mostra la CardLayout del Gioco e chiude quella del menu di pausa New Game :
+     * Mostra la CardLayout del Gioco ma dall'inizio, perdendo tutti i salvataggi
+     * fatti finora Main Menu : Mostrerà il CardLayout del Menù Principale (tutti i
+     * salvataggi verranno persi anche in questo caso) Quit : Chiuderà il gioco
+     * (System.Exit(0);)
+     * 
      */
-    
+
     public PauseMenu() {
-        this.setOpaque(false);
+        try {
+            image = ImageIO.read(getClass().getResource("/resources/pause/Background.jpg"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // this.setOpaque(false);
         JButton resume = createJB("Resume");
         JButton newgame = createJB("New Game");
         JButton mainmenu = createJB("Main Menu");
         JButton quit = createJB("Quit");
-        JLabel pausetext =  labeltext("Pause Menu");
-        
+        JLabel pausetext = labeltext("Pause Menu");
+
         this.setLayout(new GridBagLayout());
         this.add(pausetext, setButtonPosition(0, 70, 30));
         this.add(resume, setButtonPosition(1, 30, 30));
         this.add(newgame, setButtonPosition(2, 30, 30));
         this.add(mainmenu, setButtonPosition(3, 30, 30));
         this.add(quit, setButtonPosition(4, 30, 30));
-        resume.addActionListener(new ResumeAction());
-        newgame.addActionListener(new NewGameAction());
-        mainmenu.addActionListener(new MainMenuAction(null));
-        quit.addActionListener(new QuitAction());     
+        quit.addActionListener(this);
+
     }
-    
+
     private JButton createJB(String name) {
         JButton temp = new JButton(name);
-        temp.setFont(Constant.genericFont("Arial", Font.PLAIN, Constant.horizontalAspectRatio(40)));
-        temp.setPreferredSize(new Dimension(Constant.horizontalAspectRatio(350),Constant.verticalAspectRatio(150)));
+        temp.setFont(Constant.genericFont("Tahoma", Font.PLAIN, Constant.horizontalAspectRatio(40)));
+        temp.setPreferredSize(new Dimension(Constant.horizontalAspectRatio(350), Constant.verticalAspectRatio(150)));
+        temp.setBackground(new Color(197, 199, 196));
+        temp.setBorder(
+                BorderFactory.createLineBorder(new Color(67, 59, 103), Constant.horizontalAspectRatio(10), false));
         return temp;
     }
-    
-   private JLabel labeltext(String name) {
-       JLabel pause = new JLabel(name, SwingConstants.CENTER);
-       pause.setForeground(Color.white);
-       pause.setPreferredSize(new Dimension(Constant.horizontalAspectRatio(500),Constant.verticalAspectRatio(100)));
-       pause.setFont(Constant.genericFont("Arial", Font.PLAIN, Constant.horizontalAspectRatio(80)));
-       return pause;
-   }
-    
-    private GridBagConstraints setButtonPosition (int gridy, int topPad, int botPad) {
+
+    private JLabel labeltext(String name) {
+        JLabel pause = new JLabel(name, SwingConstants.CENTER);
+        pause.setForeground(Color.white);
+        pause.setPreferredSize(new Dimension(Constant.horizontalAspectRatio(500), Constant.verticalAspectRatio(100)));
+        pause.setFont(Constant.genericFont("Arial", Font.PLAIN, Constant.horizontalAspectRatio(80)));
+        return pause;
+    }
+
+    private GridBagConstraints setButtonPosition(int gridy, int topPad, int botPad) {
         GridBagConstraints a = new GridBagConstraints();
         topPad = Constant.verticalAspectRatio(topPad);
         botPad = Constant.verticalAspectRatio(botPad);
@@ -70,6 +76,13 @@ public class PauseMenu extends JPanel implements ActionListener{
     }
 
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub        
+        System.exit(0);
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(ImageModifier.scaleFullScreen(image), 0, 0, null);
+    }
+
+        
 }
