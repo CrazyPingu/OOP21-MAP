b578090887bd5740b5d23dd7ae40d24e3af05dee
Sara Romeo <sara.romeo999@gmail.com>
2022-04-09 18:48:53 +0200

    Create class to set labels' appareance

---
+++ src/view/game/logStats/LabelAppareance.java
@@ -0,0 +1,19 @@
+package view.game.logStats;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class LabelAppareance extends JLabel {
+	
+	private static final long serialVersionUID = 7460837470380147330L;
+
+	/**
+	 * Sets the appareance of stats and log labels
+	 */
+	public void appareance () {
+		this.setOpaque(true);
+        this.setBackground(Color.BLACK);
+        this.setForeground(Color.WHITE);
+        this.setFont(new Font("arial", Font.PLAIN, 20));
+	}
+}
