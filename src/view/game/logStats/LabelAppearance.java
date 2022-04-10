e0e497dc3291905d89d40100c5dbb9da19f69b81
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-10 20:38:13 +0200

    Change javadoc comment

--- src/view/game/logStats/LabelAppearance.java
+++ src/view/game/logStats/LabelAppearance.java
@@ -3,13 +3,16 @@ package view.game.logStats;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * 
+ * Sets the appearance of stats and log labels
+ *
+ */
+
 public class LabelAppearance extends JLabel {
 	
 	private static final long serialVersionUID = 7460837470380147330L;
 
-	/**
-	 * Sets the appearance of stats and log labels
-	 */
 	public void setAppearance () {
 		this.setOpaque(true);
         this.setBackground(Color.BLACK);
