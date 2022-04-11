7ec5d58b3b9e229db4f161c3f1735774541e534c
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-11 22:29:26 +0200

    Add ScrollableStats and ScrollPanelAppearance classes

---
+++ src/view/game/logStats/ScrollPaneAppearance.java
@@ -0,0 +1,11 @@
+package view.game.logStats;
+
+import javax.swing.JScrollPane;
+
+public class ScrollPaneAppearance extends JScrollPane {
+
+	private static final long serialVersionUID = 7085461478629284287L;
+	
+	
+
+}
---
+++ src/view/game/logStats/ScrollableStats.java
@@ -0,0 +1,8 @@
+package view.game.logStats;
+
+public class ScrollableStats extends ScrollPaneAppearance {
+
+	private static final long serialVersionUID = 3192980786022666161L;
+	
+
+}
