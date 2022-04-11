53c1cf539bb6fa63ccc657b225594c31a5c2019b
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-11 15:52:03 +0200

    Create variable in RoomFactoryImpl that rappresent the max number of the cells that the room will have

--- src/logics/room/RoomFactoryImpl.java
+++ src/logics/room/RoomFactoryImpl.java
@@ -1,12 +1,16 @@
 package logics.room;
 
+import utilis.Constant;
+
 /**
  * 
  * Factory to create new rooms.
  *
  */
 public class RoomFactoryImpl implements RoomFactory {
-	
+
+	private static final int x = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(64);
+	private static final int y = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(72);
 
 	/**
 	 * {@inheritDoc}
