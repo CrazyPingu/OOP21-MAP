5de4a9c6be25a81fc71313e5699dd0c1fe1cdf80
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-11 22:47:40 +0200

    Implement JTextPane appearance

--- src/view/game/logStats/TextAreaAppearance.java
+++ src/view/game/logStats/TextAreaAppearance.java
@@ -2,6 +2,7 @@ package view.game.logStats;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 import utilis.Constant;
 
@@ -11,9 +12,33 @@ import utilis.Constant;
  *
  */
 
-public class TextAreaAppearance extends JLabel {
-	
+public class TextAreaAppearance extends JTextPane {
+
 	private static final long serialVersionUID = 7460837470380147330L;
-	
-	
+
+	/**
+	 * Sets appearance of the JTextPane
+	 */
+
+	public TextAreaAppearance() {
+		super.setEditable(false);
+		super.setBorder(generateBorder(20, 10, 20, 0));
+		super.setOpaque(false);
+		super.setForeground(Color.WHITE);
+		super.setFont(Constant.genericFont("Arial", Font.PLAIN, 20));
+		this.setHighlighter(null);
+	}
+
+	/**
+	 * @param top    : top padding value to be scaled
+	 * @param left   : left padding value to be scaled
+	 * @param bottom : bottom padding value to be scaled
+	 * @param right  : right padding value to be scaled
+	 * @return an EmptyBorder to set JTextPane padding.
+	 */
+
+	private EmptyBorder generateBorder(int top, int left, int bottom, int right) {
+		return new EmptyBorder(Constant.verticalAspectRatio(top), Constant.horizontalAspectRatio(left),
+				Constant.verticalAspectRatio(bottom), Constant.horizontalAspectRatio(right));
+	}
 }
