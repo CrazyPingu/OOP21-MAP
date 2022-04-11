a79463eece8b17fb5fc42ddc22752555167ad12f
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-11 23:21:19 +0200

    Implement stats scrollPane

--- src/view/game/logStats/ScrollableStats.java
+++ src/view/game/logStats/ScrollableStats.java
@@ -1,8 +1,23 @@
 package view.game.logStats;
 
+import logics.entity.Player;
+
 public class ScrollableStats extends ScrollPaneAppearance {
 
 	private static final long serialVersionUID = 3192980786022666161L;
-	
 
+	private static Stats statsValues = new Stats();
+
+	public ScrollableStats(Player player) {
+		super(statsValues);
+		statsValues.update(player);
+		super.setScrollPaneAppearance();
+	}
+
+	/**
+	 * @return stats values implemented as JTextPane.
+	 */
+	public static Stats getStatsValues() {
+		return statsValues;
+	}
 }
