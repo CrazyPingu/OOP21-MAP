76f08dae60cea71aad1dcb72f2350108c4835061
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-11 23:23:34 +0200

    Implement Log scrollPane

--- src/view/game/logStats/ScrollableLog.java
+++ src/view/game/logStats/ScrollableLog.java
@@ -1,47 +1,20 @@
 package view.game.logStats;
 
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.border.Border;
-
-import utilis.Constant;
-
-public class ScrollableLog extends JScrollPane {
+public class ScrollableLog extends ScrollPaneAppearance {
 
 	private static final long serialVersionUID = -2513174390328036056L;
-	
-	private static Log logMessage = new Log ();
-	Border border;
 
-	public ScrollableLog () {
+	private static Log logMessage = new Log();
+
+	public ScrollableLog() {
 		super(logMessage);
-		this.setScroll();
+		this.setScrollPaneAppearance();
 	}
 	
 	/**
-	 * Sets the appearance of the scroll pane.
+	 * @return log message implemented as JTextPane.
 	 */
-	public void setScroll () {        
-		this.setScrollBars();
-		super.setBackground(Color.BLACK);
-		super.setPreferredSize(new Dimension (Constant.WIDTH / 4,  Constant.HEIGHT * 2 / 3));
-		border = BorderFactory.createLineBorder(Color.GRAY,3);
-        this.setBorder(border);
-	}
-	
-	/**
-	 * Sets the appearance of the scroll bars.
-	 */
-	public void setScrollBars () {
-		super.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		super.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		super.getVerticalScrollBar().setUnitIncrement(15);
-		super.getVerticalScrollBar().setBackground(Color.BLACK);
-		super.getHorizontalScrollBar().setBackground(Color.BLACK);
-	}
-	
-	
-	public Log getLogMessage() {
+	public static Log getLogMessage() {
 		return logMessage;
 	}
 
