fd0ecb17aa225c083c56e6a651bcdae426d5cce1
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-12 00:43:48 +0200

    Fix GenerateRandomEnemyMap method

--- src/logics/room/GenerateRandomEnemyMap.java
+++ src/logics/room/GenerateRandomEnemyMap.java
@@ -8,25 +8,32 @@ import utilis.Pair;
 public class GenerateRandomEnemyMap extends HashMap<Pair<Integer, Integer>, Entity> {
 
 	private static final long serialVersionUID = -1346040616337955961L;
-	private static final EnemyFactoryImpl enemyFactory = new EnemyFactoryImpl();
-	private static int possibleZombieNumber = enemyFactory.getClass().getDeclaredMethods().length;
+	private final int spawningRatio = 30;
+	private final int forbiddenZombieSpawn = 3;
+	private final EnemyFactoryImpl enemyFactory = new EnemyFactoryImpl();
+	private final int possibleZombieNumber = enemyFactory.getClass().getDeclaredMethods().length;
 
 	public GenerateRandomEnemyMap(Pair<Integer, Integer> cells) {
-		for (int i = 0; i < cells.getX() * cells.getY() / 30; i++) {
-			this.put(new Pair<Integer, Integer>(((int) Math.random() * cells.getX() - 3) + 3,
-					((int) Math.random() * cells.getY())), generateRandomEnemy());
+		Pair<Integer, Integer> zombieSpawn;
+		for (int i = 0; i < cells.getX() * cells.getY() / spawningRatio; i++) {
+			do {
+				zombieSpawn = new Pair<Integer, Integer>(
+						((int) Math.random() * cells.getX() - forbiddenZombieSpawn) + forbiddenZombieSpawn,
+						((int) Math.random() * cells.getY()));
+			} while (!this.containsKey(zombieSpawn));
+			this.put(zombieSpawn, generateRandomEnemy());
 		}
 	}
 
 	private Entity generateRandomEnemy() {
 		int random = (int) Math.random() * possibleZombieNumber;
 		Entity generatedEnemy = null;
-			try {
-				generatedEnemy = (Entity)enemyFactory.getClass().getDeclaredMethods()[random].invoke(enemyFactory);
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println("Error enemy generation");
-			}
+		try {
+			generatedEnemy = (Entity) enemyFactory.getClass().getDeclaredMethods()[random].invoke(enemyFactory);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error enemy generation");
+		}
 		return generatedEnemy;
 	}
 
