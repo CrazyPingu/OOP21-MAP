fc4613ebf7f9bf4de9411af84a21b728803d9755
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-16 14:29:40 +0200

    create interface for MovementFactory

---
+++ src/logics/strategy/movement/MovementFactory.java
@@ -0,0 +1,28 @@
+package logics.strategy.movement;
+
+
+/**
+ * Interface that declares methods for the Movement Factory. Each method returns
+ * a different type of movement.
+ * 
+ */
+public interface MovementFactory {
+
+	/**
+	 * 
+	 * @param actioNum represent the number of action the entity can perform in a
+	 *                 turn
+	 * @return a MovementImpl that allow an an entity to move only in the adiacents
+	 *         cells
+	 */
+	public MovementImpl stepMovement(int actioNum);
+
+	/**
+	 * 
+	 * @param actioNum represent the number of action the entity can perform in a
+	 *                 turn.
+	 * @return a MovementImpl that allow an entity to move in a "+" shape cells with
+	 *         a distance of 1 from his current position. 
+	 */
+	public MovementImpl jumpMovement(int actioNum);
+}
