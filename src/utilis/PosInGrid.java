76154287c41f401ebed9b9b02acd3c6af3326be1
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-16 19:27:25 +0200

    changes in java docs

--- src/utilis/PosInGrid.java
+++ src/utilis/PosInGrid.java
@@ -8,10 +8,11 @@ public class PosInGrid {
 	/**
 	 * 
 	 * @param pos  represent the position to check
-	 * @param size represent the grid size
+	 * @param size represents the grid size. First cordinate determinate the
+	 *             orizontal size (1 based) and second ones the vertical size.
 	 * @return true if the position is in the grid, false if it isn't
 	 */
 	public static final boolean checkPosInGrid(Pair<Integer, Integer> pos, Pair<Integer, Integer> size) {
-		return pos.getX() < size.getX() ? (pos.getY() < size.getY()? true: false) : false;
+		return pos.getX() < size.getX() ? (pos.getY() < size.getY() ? true : false) : false;
 	}
 }
