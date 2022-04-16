8c5443a031b638e4c1e0e4b22c657c411ba9ae23
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-16 12:15:12 +0200

    Create implementation of WeaponFactory interface

---
+++ src/logics/weapon/WeaponFactoryImpl.java
@@ -0,0 +1,15 @@
+package logics.weapon;
+
+import logics.weapon.shootingStrategy.ShootingStrategy;
+
+public class WeaponFactoryImpl implements WeaponFactory {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Weapon createWeapon(int damage, ShootingStrategy shootingStrategy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
