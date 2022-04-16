ecc30cf56e21aba2ac6d33999eb065b49668cee7
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-16 16:44:31 +0200

    finish stepMovement method

--- src/logics/strategy/movement/MovementFactoryImpl.java
+++ src/logics/strategy/movement/MovementFactoryImpl.java
@@ -1,9 +1,11 @@
 package logics.strategy.movement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import logics.strategy.Strategy;
 import utilis.Pair;
+import utilis.PosInGrid;
 
 public class MovementFactoryImpl implements MovementFactory {
 
@@ -13,8 +15,16 @@ public class MovementFactoryImpl implements MovementFactory {
 
 			@Override
 			public List<Pair<Integer, Integer>> execute(Pair<Integer, Integer> pos, Pair<Integer, Integer> size) {
-				// TODO Auto-generated method stub
-				return null;
+				List<Pair<Integer, Integer>> availableCell = new ArrayList<Pair<Integer,Integer>>(); 
+				for(int i = pos.getX()-1;  i < pos.getX()+1; i++) { 
+					for(int j = pos.getY()-1; j < pos.getY()+1; j++) { 
+						Pair<Integer, Integer> cellToAdd = new Pair<>(i, j); 
+						if(!pos.equals(cellToAdd) && PosInGrid.checkPosInGrid(cellToAdd, size)) { 
+							availableCell.add(cellToAdd); 
+						}
+					}
+				}
+				return availableCell;
 			}
 		}, actioNum);
 	}
@@ -30,5 +40,6 @@ public class MovementFactoryImpl implements MovementFactory {
 			}
 		}, actioNum);
 	}
+	
 
 }
