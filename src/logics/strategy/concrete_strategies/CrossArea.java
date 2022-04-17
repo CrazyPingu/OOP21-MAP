a224024f898b846919306b94a979707ebe81e053
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-17 19:45:58 +0200

    add method description

--- src/logics/strategy/concrete_strategies/CrossArea.java
+++ src/logics/strategy/concrete_strategies/CrossArea.java
@@ -10,24 +10,28 @@ import utilis.PosInGrid;
 public class CrossArea implements Strategy {
 
 	@Override
+	/**
+	 * Concrete strategy. Implements an algotithm that return the cross shape area
+	 * around the given coordinate.
+	 */
 	public List<Pair<Integer, Integer>> execute(Pair<Integer, Integer> pos, Pair<Integer, Integer> size) {
 		List<Pair<Integer, Integer>> reachableCells = new ArrayList<Pair<Integer, Integer>>();
-		Pair<Integer, Integer> cellToAdd; 
-		
+		Pair<Integer, Integer> cellToAdd;
+
 		for (int i = pos.getX() - 2; i <= pos.getX() + 2; i++) {
 			cellToAdd = new Pair<>(i, pos.getY());
 			if (!pos.equals(cellToAdd) && PosInGrid.checkPosInGrid(cellToAdd, size)) {
 				reachableCells.add(cellToAdd);
 			}
 		}
-	
+
 		for (int i = pos.getY() - 2; i <= pos.getY() + 2; i++) {
 			cellToAdd = new Pair<>(pos.getX(), i);
 			if (!pos.equals(cellToAdd) && PosInGrid.checkPosInGrid(cellToAdd, size)) {
 				reachableCells.add(cellToAdd);
 			}
 		}
-	
+
 		return reachableCells;
 	}
 
