bf4245b6b0430bb3952315cc364ecb5584b7faf3
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-17 19:50:07 +0200

    implement method runMovement

--- src/logics/strategy/movement/MovementFactoryImpl.java
+++ src/logics/strategy/movement/MovementFactoryImpl.java
@@ -1,10 +1,7 @@
 package logics.strategy.movement;
 
-import java.util.ArrayList;
-import java.util.List;
-import logics.strategy.Strategy;
+import logics.strategy.concrete_strategies.CrossArea;
 import logics.strategy.concrete_strategies.aroundArea;
-import utilis.Pair;
 
 public class MovementFactoryImpl implements MovementFactory {
 
@@ -14,17 +11,8 @@ public class MovementFactoryImpl implements MovementFactory {
 	}
 
 	@Override
-	public MovementImpl jumpMovement(int actioNum) {
-		return new MovementImpl(new Strategy() {
-
-			@Override
-			public List<Pair<Integer, Integer>> execute(Pair<Integer, Integer> pos, Pair<Integer, Integer> size) {
-
-				List<Pair<Integer, Integer>> availableCell = new ArrayList<Pair<Integer, Integer>>();
-
-				return null;
-			}
-		}, actioNum);
+	public MovementImpl runMovement(int actioNum) {
+		return new MovementImpl(new CrossArea(), actioNum);
 	}
 
 }
