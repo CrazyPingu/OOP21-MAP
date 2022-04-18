f45f7fda5dd4b480f90918105ea1df36423ac62f
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-18 17:30:23 +0200

    Change damage values, create tube weapon

--- src/logics/weapon/WeaponFactoryImpl.java
+++ src/logics/weapon/WeaponFactoryImpl.java
@@ -1,28 +1,27 @@
 package logics.weapon;
 
-import java.util.List;
-
 import logics.strategy.concrete_strategies.*;
 
 public class WeaponFactoryImpl implements WeaponFactory {
 	
-	private static int STICK_DAMAGE = 10;
-	private static int AX_DAMAGE = 20;
-	private static int DAGGER_DAMAGE = 30;
-	private static int GUN_DAMAGE = 40;
+	private static int STICK_DAMAGE = 1;
+	private static int TUBE_DAMAGE = 2;
+	private static int AX_DAMAGE = 3;
+	private static int DAGGER_DAMAGE = 3;
+	private static int GUN_DAMAGE = 4;
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public Weapon createStick() {
-		return new WeaponImpl(STICK_DAMAGE, new AroundArea());
+		return new WeaponImpl(STICK_DAMAGE, new AroundArea(VariableDistanceConstants.SINGLE_DISTANCE));
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public Weapon createAx() {
-		return new WeaponImpl(AX_DAMAGE, new AroundArea());
+		return new WeaponImpl(AX_DAMAGE, new AroundArea(VariableDistanceConstants.SINGLE_DISTANCE));
 	}
 
 	/**
@@ -35,8 +34,15 @@ public class WeaponFactoryImpl implements WeaponFactory {
 	/**
 	 * {@inheritDoc}
 	 */
+	public Weapon createTube() {
+		return new WeaponImpl(TUBE_DAMAGE, /*new AsteriskArea()*/);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	public Weapon createGun() {
-		return new WeaponImpl(GUN_DAMAGE, new AroundArea());
+		return new WeaponImpl(GUN_DAMAGE, new AroundArea(VariableDistanceConstants.DOUBLE_DISTANCE));
 	}
 	
 }
