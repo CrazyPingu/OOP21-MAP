2a3faf821e60e3016dd169ec2d783ba452ae4d58
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-18 18:51:03 +0200

    Implement method to get the selectable area

--- src/logics/strategy/concrete_strategies/AsteriskArea.java
+++ src/logics/strategy/concrete_strategies/AsteriskArea.java
@@ -1,9 +1,11 @@
 package logics.strategy.concrete_strategies;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import logics.strategy.Strategy;
 import utilis.Pair;
+import utilis.PosInGrid;
 
 /**
  * 
@@ -13,8 +15,31 @@ import utilis.Pair;
  */
 public class AsteriskArea implements Strategy {
 	
+	private int distance;
+
+	public AsteriskArea(int distance) {
+		this.distance = distance;
+	}
+	
 	@Override
+	/**
+	 * {inheritDoc}
+	 */
 	public List<Pair<Integer, Integer>> execute(Pair<Integer, Integer> pos, Pair<Integer, Integer> size) {
-		return null;
+		List<Pair<Integer, Integer>> reachableCells = new ArrayList<Pair<Integer, Integer>>();		
+		AroundArea aroundArea = new AroundArea(VariableDistanceConstants.SINGLE_DISTANCE);
+		List<Pair<Integer, Integer>> aroundAreaList = aroundArea.execute(pos, size);
+		
+		for (int i = pos.getX() - this.distance; i <= pos.getX() + this.distance; i++) {
+			for (int j = pos.getY() - this.distance; j <= pos.getY() + this.distance; j++) {
+				Pair<Integer, Integer> cellToAdd = new Pair<>(i, j);
+				if (!aroundAreaList.contains(cellToAdd) && !pos.equals(cellToAdd) && PosInGrid.checkPosInGrid(cellToAdd, size)) {
+						reachableCells.add(cellToAdd);
+				}
+			}
+		}
+		
+		reachableCells.addAll(aroundAreaList);
+		return reachableCells;
 	}
 }
