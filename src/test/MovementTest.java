8ef439da88f3c0a37664c62343d0ad65e533dc7c
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-18 14:27:56 +0200

    created class for movement creation testing

---
+++ src/test/MovementTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import logics.strategy.concrete_strategies.CrossArea;
+import logics.strategy.concrete_strategies.aroundArea;
+import logics.strategy.movement.Movement;
+import logics.strategy.movement.MovementImpl;
+
+public class MovementTest {
+	private Movement movement; 
+	private final int NUMBER_ACTION = 1;  
+	@Test
+	/*
+	 * test the correct creation of a movement class
+	 */
+	void createMovementTest() {
+		this.movement = new MovementImpl(new aroundArea(), NUMBER_ACTION);
+		assertTrue(this.movement.getActionNumber() == NUMBER_ACTION);
+		
+		this.movement = new MovementImpl(new CrossArea(), NUMBER_ACTION);
+		assertTrue(this.movement.getActionNumber() == NUMBER_ACTION);
+	}
+	
+	
+}
