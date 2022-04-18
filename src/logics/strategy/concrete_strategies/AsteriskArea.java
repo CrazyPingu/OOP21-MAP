4c3b20f5d57b47ecf08a7398e366093764c476a1
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-18 17:38:22 +0200

    Create asterisk area strategy

---
+++ src/logics/strategy/concrete_strategies/AsteriskArea.java
@@ -0,0 +1,20 @@
+package logics.strategy.concrete_strategies;
+
+import java.util.List;
+
+import logics.strategy.Strategy;
+import utilis.Pair;
+
+/**
+ * 
+ * Concrete strategy. Implements an algorithm that returns an asterisk shaped area
+ * around the given coordinate.
+ * 
+ */
+public class AsteriskArea implements Strategy {
+	
+	@Override
+	public List<Pair<Integer, Integer>> execute(Pair<Integer, Integer> pos, Pair<Integer, Integer> size) {
+		return null;
+	}
+}
