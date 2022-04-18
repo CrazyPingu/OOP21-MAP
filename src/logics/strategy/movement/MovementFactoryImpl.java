5d4b9c83459f3be673da3b4e923d1a48a278f579
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-18 17:11:08 +0200

    Fix based on variable range

--- src/logics/strategy/movement/MovementFactoryImpl.java
+++ src/logics/strategy/movement/MovementFactoryImpl.java
@@ -1,13 +1,14 @@
 package logics.strategy.movement;
 
 import logics.strategy.concrete_strategies.CrossArea;
+import logics.strategy.concrete_strategies.VariableDistanceConstants;
 import logics.strategy.concrete_strategies.AroundArea;
 
 public class MovementFactoryImpl implements MovementFactory {
 
 	@Override
 	public MovementImpl stepMovement(int actioNum) {
-		return new MovementImpl(new AroundArea(), actioNum);
+		return new MovementImpl(new AroundArea(VariableDistanceConstants.SINGLE_DISTANCE), actioNum);
 	}
 
 	@Override
