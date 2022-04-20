65e5903ebd54829b624b3402b73cfe15f9a756ee
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-20 15:44:43 +0200

    Add javaDoc in ImageMethod

--- src/utilis/ImageMethod.java
+++ src/utilis/ImageMethod.java
@@ -4,9 +4,17 @@ import java.awt.Image;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-
+/**
+ * 
+ * Class to work easily with images
+ *
+ */
 public class ImageMethod {
 
+	/**
+	 * @param fileName : the name of the images (and his path from resources)
+	 * @return the image correlated
+	 */
 	public static Image getImage(String fileName) {
 		try {
 			return ImageIO.read(ImageMethod.class.getResource("/resources/" + fileName));
@@ -16,6 +24,10 @@ public class ImageMethod {
 		return null;
 	}
 
+	/**
+	 * @param fileName : the name of the images (and his path from resources)
+	 * @return the ImageIcon
+	 */
 	public static ImageIcon getImageIcon(String fileName) {
 		return new ImageIcon(getImage(fileName));
 	}
