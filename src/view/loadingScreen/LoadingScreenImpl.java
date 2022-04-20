a6262da5c326e1ebb32f9d07759b4d8a4937b53b
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-20 17:29:04 +0200

    Fix imported libraries

--- src/view/loadingScreen/LoadingScreenImpl.java
+++ src/view/loadingScreen/LoadingScreenImpl.java
@@ -1,12 +1,20 @@
 package view.loadingScreen;
 
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingConstants;
 import utilis.Constant;
 import utilis.GbcDimension;
 import utilis.ImageMethod;
 import utilis.ImageModifier;
 import view.frame.BasicFrame;
-import java.awt.*;
 
 /**
  * 
