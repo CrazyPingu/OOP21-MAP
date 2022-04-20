726af358d98b105e74d2ba1618afdf1223e5e46c
Barto00 <federico.bartolini6@studio.unibo.it>
2022-04-20 15:54:59 +0200

    change getImage in PauseMenu

--- src/view/pause/PauseMenu.java
+++ src/view/pause/PauseMenu.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import utilis.Constant;
+import utilis.ImageMethod;
 import utilis.ImageModifier;
 import view.frame.BasicFrame;
 import view.pause.action.MainMenuAction;
@@ -31,29 +32,22 @@ public class PauseMenu extends JPanel {
      * @param frame : the frame passed in input, change the screen using buttons
      */
     public PauseMenu(BasicFrame frame) {
-        try {
-            image = ImageIO.read(getClass().getResource("/resources/pause/Background.jpg"));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    
+        image = ImageMethod.getImage("pause/Background.jpg");
         JButton resume = createJB("Resume");
-        JButton newgame = createJB("New Game");
         JButton mainmenu = createJB("Main Menu");
         JButton quit = createJB("Quit");
         JLabel pausetext = labeltext("Pause Menu");
 
         this.setLayout(new GridBagLayout());
-        this.add(pausetext, setButtonPosition(0, Constant.horizontalAspectRatio(70), Constant.verticalAspectRatio(30)));
-        this.add(resume, setButtonPosition(1, Constant.horizontalAspectRatio(30), Constant.verticalAspectRatio(30)));
-        this.add(newgame, setButtonPosition(2, Constant.horizontalAspectRatio(30), Constant.verticalAspectRatio(30)));
-        this.add(mainmenu, setButtonPosition(3, Constant.horizontalAspectRatio(30), Constant.verticalAspectRatio(30)));
-        this.add(quit, setButtonPosition(4, Constant.horizontalAspectRatio(30), Constant.verticalAspectRatio(30)));
+//        this.add(pausetext, setButtonPosition(0, Constant.horizontalAspectRatio(70), Constant.verticalAspectRatio(30)));
+//        this.add(resume, setButtonPosition(1, Constant.horizontalAspectRatio(30), Constant.verticalAspectRatio(30)));
+//        this.add(mainmenu, setButtonPosition(3, Constant.horizontalAspectRatio(30), Constant.verticalAspectRatio(30)));
+//        this.add(quit, setButtonPosition(4, Constant.horizontalAspectRatio(30), Constant.verticalAspectRatio(30)));    
         resume.addActionListener(new ResumeAction(frame));
-        newgame.addActionListener(new NewGameAction(frame));
         mainmenu.addActionListener(new MainMenuAction(frame));
         quit.addActionListener(new QuitAction());
-
+        
     }
 
     /**
@@ -83,15 +77,15 @@ public class PauseMenu extends JPanel {
         pause.setFont(Constant.genericFont("Arial", Font.PLAIN, Constant.horizontalAspectRatio(80)));
         return pause;
     }
-        
-    private GridBagConstraints setButtonPosition(int gridy, int topPad, int botPad) {
-        GridBagConstraints a = new GridBagConstraints();
-        topPad = Constant.verticalAspectRatio(topPad);
-        botPad = Constant.verticalAspectRatio(botPad);
-        a.gridy = gridy;
-        a.insets = new Insets(topPad, 0, botPad, 0);
-        return a;
-    }
+//        
+//    private GridBagConstraints setButtonPosition(int gridy, int topPad, int botPad) {
+//        GridBagConstraints a = new GridBagConstraints();
+//        topPad = Constant.verticalAspectRatio(topPad);
+//        botPad = Constant.verticalAspectRatio(botPad);
+//        a.gridy = gridy;
+//        a.insets = new Insets(topPad, 0, botPad, 0);
+//        return a;
+//    }
 
     /**
      * {@inheritDoc}
