373c2555e6de79432de2f42460685150154ba10a
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-21 14:16:20 +0200

    Create the panel at the top of the game

---
+++ src/view/game/TopPanel.java
@@ -0,0 +1,15 @@
+package view.game;
+
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+
+public class TopPanel extends JPanel{
+
+	private static final long serialVersionUID = 3295995931719233232L;
+
+	public TopPanel() {
+		this.setLayout(new GridBagLayout());
+		this.setOpaque(false);
+	}
+
+}
