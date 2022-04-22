c2d950f75a431c79ff16805423e6c07754d1c776
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-22 14:54:39 +0200

    Create GameButton

---
+++ src/view/game/central/GameButton.java
@@ -0,0 +1,28 @@
+package view.game.central;
+
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+public class GameButton extends JButton implements ActionListener {
+
+	private static final long serialVersionUID = 1384166202851332499L;
+
+	public GameButton() {
+
+	}
+	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
