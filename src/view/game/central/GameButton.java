f74deaa9f45c58b7b035d682066c47db443fc24f
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-22 15:17:43 +0200

    Create GameButton and add resources

--- src/view/game/central/GameButton.java
+++ src/view/game/central/GameButton.java
@@ -5,16 +5,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
+import utilis.ImageMethod;
+import utilis.ImageModifier;
+
 public class GameButton extends JButton implements ActionListener {
 
 	private static final long serialVersionUID = 1384166202851332499L;
 
 	public GameButton() {
-
+		
 	}
 	
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		g.drawImage(ImageModifier.scale(ImageMethod.getImage("room/Button.png"), getSize()), 0, 0, null);
 
 	}
 	
