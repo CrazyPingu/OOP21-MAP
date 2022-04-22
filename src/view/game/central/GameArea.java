0faf27b522e94c33aa2a0bd4e8bda05e7053f394
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-04-22 16:50:22 +0200

    Add JavaDoc

--- src/view/game/central/GameArea.java
+++ src/view/game/central/GameArea.java
@@ -6,6 +6,11 @@ import javax.swing.JPanel;
 import logics.room.works.Room;
 import utilis.Pair;
 
+/**
+ * 
+ * Class to manage to central area of the game view
+ *
+ */
 public class GameArea extends JPanel {
 
 	private static final long serialVersionUID = 2941617427011748438L;
@@ -20,16 +25,22 @@ public class GameArea extends JPanel {
 		placeCells();
 	}
 
+	/**
+	 * Method to place the GameButton inside the GameArea
+	 */
 	private void placeCells() {
 		for (int i = 0; i < size.getX(); i++) {
 			for (int j = 0; j < size.getY(); j++) {
-				final GameButton jb = new GameButton(" ");
+				final GameButton jb = new GameButton();
 				room.addToCells(new Pair<>(j, i), jb);
 				this.add(jb);
 			}
 		}
 	}
-	
+
+	/**
+	 * Method to remove all the objects from the room
+	 */
 	public void clearButton() {
 		room.getCells().entrySet().forEach(x -> x.getValue().setSprite(new JLabel("")));
 	}
--- src/view/game/central/GameButton.java
+++ src/view/game/central/GameButton.java
@@ -11,19 +11,25 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import utilis.ImageMethod;
-
+/**
+ * 
+ * Class to manage the single button of the GameArea
+ *
+ */
 public class GameButton extends JButton implements ActionListener {
 
 	private static final long serialVersionUID = 1384166202851332499L;
 	Graphics2D g2;
 	private JLabel sprite;
 	
-	public GameButton(String text) {
-		super(text);
+	public GameButton() {
 		sprite = new JLabel("");
 		this.add(sprite);
 	}
 
+	/**
+	 * Method to draw the background of the GameButton
+	 */
 	protected void paintComponent(Graphics g) {
 		g2 = (Graphics2D) g;
 		g2.drawImage(ImageMethod.getImage("room/Button.png"), 0, 0, (int) getSize().getWidth(),
@@ -31,19 +37,29 @@ public class GameButton extends JButton implements ActionListener {
 
 	}
 	
+	/**
+	 * @param object paints the image of the object
+	 */
 	public void drawGameObject(GameObject object) {
 		sprite = new JLabel(ImageMethod.getImageIcon(object.getImagePath()));
 	}
 
+	
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 
 	}
-	
+
+	/**
+	 * @return the label sprite
+	 */
 	public JLabel getSprite() {
 		return sprite;
 	}
 	
+	/**
+	 * @param label change the label sprite
+	 */
 	public void setSprite(JLabel label) {
 		this.sprite = label;
 	}
