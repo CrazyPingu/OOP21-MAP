620f581d83195ad9f9175647fc617c65973a95d9
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-23 18:06:35 +0200

    changed interface in to an abstract class. In that way the is possible decrese the repeated code

--- src/logics/game_object/artefact/Artefact.java
+++ src/logics/game_object/artefact/Artefact.java
@@ -1,18 +1,51 @@
 package logics.game_object.artefact;
 
-import logics.entity.Player;
+import logics.game_object.GameObject;
+import logics.game_object.entity.Player;
+import utilis.Pair;
 
 /**
  * 
  * This interface declare the common method for every artefact. 
  *
  */
-public interface Artefact {
+public abstract class Artefact implements GameObject {
 
+	private Pair<Integer, Integer> pos; 
+	private String name; 
+	private String texturePath; 
+	
+	
+	public Artefact(Pair<Integer, Integer> pos, String name, String texturePath ) {
+		this.pos = pos;
+		this.name = name;
+		this.texturePath = texturePath; 
+	}
+
+	public Pair<Integer, Integer> getPos(){
+		return this.pos; 
+	}
+	
+	/**
+	 * 
+	 * @return the name of the GameObject
+	 */
+	public String getName() {
+		return this.name; 
+	} 
+	
+	/**
+	 * 
+	 * @return the object's texture path
+	 */
+	public String getImagePath() {
+		return this.texturePath;
+	} 
+		
 	/**
 	 * 
 	 * @param player represent the player to which the effect will be applied
 	 */
-	public void execute(Player player); 
+	public abstract void execute(Player player); 
 	
 }
