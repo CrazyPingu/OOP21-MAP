f89eeacb6f1ad640bf64e5183b538e5bc36beee2
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-24 11:47:43 +0200

    implements method

--- src/logics/life/ExtendibleMaxLifeSystem.java
+++ src/logics/life/ExtendibleMaxLifeSystem.java
@@ -1,17 +1,44 @@
 package logics.life;
 
-public class ExtendibleMaxLifeSystem implements LifeSystem {
+public class ExtendibleMaxLifeSystem implements LifeSystem, HealSystem {
 
-	private HealLifeSystem life; 
-	private int maxHealthExtension; 
-	
+	private HealLifeSystem life;
+	private int maxHealthExtension;
+
+	/**
+	 * 
+	 * @param startingLife       is the amount of health the system start with
+	 * @param maxHealth          the maximum amount of health that can be reached
+	 *                           using heal method
+	 * @param maxHealthExtension represent the maximum extension value for the
+	 *                           health
+	 */
 	public ExtendibleMaxLifeSystem(int startingLife, int maxHealth, int maxHealthExtension) {
-		this.life = new HealLifeSystem(startingLife, maxHealth); 
+		this.life = new HealLifeSystem(startingLife, maxHealth);
 		this.maxHealthExtension = maxHealthExtension;
 	}
 
+	/**
+	 * 
+	 * @param maxLifeExtension represent the new maximum amount of health the system
+	 *                         can reach
+	 */
+	public void extendMaxLife(int lifeExtension) {
+		if (lifeExtension > 0 && (lifeExtension <= this.maxHealthExtension)) {
+			int newStartingLifeValue = this.life.getCurrentHealth() <= lifeExtension ? this.life.getCurrentHealth()
+					: lifeExtension;
+			this.life = new HealLifeSystem(newStartingLifeValue, lifeExtension);
+		}
+	}
 	
-	
+	/**
+	 * 
+	 * @return the current maximum amounth of health the sistem can reach using the heal method
+	 */
+	public int getCurrentmaxHealth() { 
+		return this.life.getMaxHealth(); 
+	}
+
 	@Override
 	public int getCurrentHealth() {
 		return this.life.getCurrentHealth();
@@ -26,6 +53,17 @@ public class ExtendibleMaxLifeSystem implements LifeSystem {
 	@Override
 	public Boolean isDead() {
 		return this.life.isDead();
+	}
+
+	@Override
+	public void heal(int healValue) {
+		this.life.heal(healValue);
+
+	}
+
+	@Override
+	public int getMaxHealth() {
+		return this.life.getMaxHealth();
 	}
 
 }
