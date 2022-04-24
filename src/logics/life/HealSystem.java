db8b46039372bfde2a72141b84ec0c231e7d0314
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-24 11:21:35 +0200

    create interface for define method in common from the LifeSystem that allow to heal

---
+++ src/logics/life/HealSystem.java
@@ -0,0 +1,18 @@
+package logics.life;
+
+public interface HealSystem {
+	
+	/**
+	 * 
+	 * @param healValue represent the amount of life to increase. The helth value
+	 *                  can't be greater than max health. Don't work with negative
+	 *                  values
+	 */
+	public void heal(int healValue); 
+
+	/**
+	 * 
+	 * @return the maximum amount of health that can be reached
+	 */
+	public int getMaxHealth(); 
+}
