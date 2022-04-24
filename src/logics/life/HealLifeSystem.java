b8d0218234793f23c7d62c362c2e1c806efed106
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-04-24 10:36:17 +0200

    bug fix, now heal works only with positives value and the method isDead() works

--- src/logics/life/HealLifeSystem.java
+++ src/logics/life/HealLifeSystem.java
@@ -19,21 +19,24 @@ public class HealLifeSystem implements LifeSystem {
 	/**
 	 * 
 	 * @param healValue represent the amount of life to increase. The helth value
-	 *                  can't be greater than max health
+	 *                  can't be greater than max health. Don't work with negative
+	 *                  values
 	 */
 	public void heal(int healValue) {
-		int newHealthValue = (this.life.getCurrentHealth() + healValue) > maxHealth ? maxHealth
-				: this.life.getCurrentHealth() + healValue;
-		this.life = new SimpleLifeSystem(newHealthValue);
+		if (healValue > 0) {
+			int newHealthValue = (this.life.getCurrentHealth() + healValue) > maxHealth ? maxHealth
+					: this.life.getCurrentHealth() + healValue;
+			this.life = new SimpleLifeSystem(newHealthValue);
+		}
 	}
-	
+
 	/**
 	 * 
 	 * @return the maximum amount of health that can be reached
 	 */
 	public int getMaxHealth() {
-		return this.maxHealth; 
-		
+		return this.maxHealth;
+
 	}
 
 	@Override
@@ -48,7 +51,7 @@ public class HealLifeSystem implements LifeSystem {
 
 	@Override
 	public Boolean isDead() {
-		return this.isDead();
+		return this.life.isDead();
 	}
 
 }
