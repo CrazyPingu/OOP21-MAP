16964eb91917e95cb879a3aa0d363bcf6f50a6a8
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-04-30 16:54:37 +0200

    Create 5 types of enemies

--- src/logics/game_object/entity/enemy/EnemyFactoryImpl.java
+++ src/logics/game_object/entity/enemy/EnemyFactoryImpl.java
@@ -1,5 +1,12 @@
 package logics.game_object.entity.enemy;
 
+import logics.game_object.entity.SimpleEntity;
+import logics.life.SimpleLifeSystem;
+import logics.strategy.movement.MovementFactoryImpl;
+import logics.strategy.weapon.WeaponFactoryImpl;
+import utilis.Pair;
+import utilis.texture.EntityTexture;
+
 /**
  * 
  * Factory to create new enemy.
@@ -10,8 +17,41 @@ public class EnemyFactoryImpl implements EnemyFactory {
 	/**
 	 * {@inheritDoc}
 	 */
-	public Entity createEnemy() {
-		// TODO Auto-generated method stub
-		return null;
+	public SimpleEntity createZombieStick(Pair<Integer, Integer> pos) {
+		return new SimpleEntity(new SimpleLifeSystem(5), pos, new WeaponFactoryImpl().createStick(),
+				new MovementFactoryImpl().stepMovement(), "zombie", EntityTexture.ZOMBIE_STICK);
 	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public SimpleEntity createZombieAxe(Pair<Integer, Integer> pos) {
+		return new SimpleEntity(new SimpleLifeSystem(5), pos, new WeaponFactoryImpl().createStick(),
+				new MovementFactoryImpl().stepMovement(), "zombie", EntityTexture.ZOMBIE_AXE);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public SimpleEntity createZombieDagger(Pair<Integer, Integer> pos) {
+		return new SimpleEntity(new SimpleLifeSystem(5), pos, new WeaponFactoryImpl().createStick(),
+				new MovementFactoryImpl().runMovement(), "zombie", EntityTexture.ZOMBIE_DAGGER);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public SimpleEntity createZombieTube(Pair<Integer, Integer> pos) {
+		return new SimpleEntity(new SimpleLifeSystem(5), pos, new WeaponFactoryImpl().createStick(),
+				new MovementFactoryImpl().stepMovement(), "zombie", EntityTexture.ZOMBIE_TUBE);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public SimpleEntity createZombieGun(Pair<Integer, Integer> pos) {
+		return new SimpleEntity(new SimpleLifeSystem(5), pos, new WeaponFactoryImpl().createStick(),
+				new MovementFactoryImpl().runMovement(), "zombie", EntityTexture.ZOMBIE_GUN);
+	}
+	
 }
