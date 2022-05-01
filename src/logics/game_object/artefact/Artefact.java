ba1c5261449d63ff5eb8e7809c454933e9899172
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-05-01 18:52:58 +0200

    modified argument of the function excecute

--- src/logics/game_object/artefact/Artefact.java
+++ src/logics/game_object/artefact/Artefact.java
@@ -1,61 +1,63 @@
 package logics.game_object.artefact;
 
+import java.awt.Image;
+
+import logics.game_object.ArtefactUserObject;
 import logics.game_object.GameObject;
-import logics.game_object.entity.Player;
 import utilis.Pair;
 
 /**
  * 
- * This interface declare the common method for every artefact. 
+ * This interface declare the common method for every artefact.
  *
  */
 public abstract class Artefact implements GameObject {
 
-	private Pair<Integer, Integer> pos; 
-	private String name; 
-	private String texturePath; 
-	
+	private Pair<Integer, Integer> pos;
+	private String name;
+	private Image textureImage;
+
 	/**
 	 * 
-	 * @param pos represent the position of the artefact in the game grid 
-	 * @param name is the artefact name 
-	 * @param texturePath is the path were is possibnle to find the texture for the artefact
+	 * @param pos  represent the position of the artefact in the game grid
+	 * @param name is the artefact name
+	 * @param the  texture of the player. Texture can be found in utilis.texture
 	 */
-	public Artefact(Pair<Integer, Integer> pos, String name, String texturePath ) {
+	public Artefact(Pair<Integer, Integer> pos, String name, Image textureImage) {
 		this.pos = pos;
 		this.name = name;
-		this.texturePath = texturePath; 
+		this.textureImage = textureImage;
 	}
-
 
 	/**
 	 * 
 	 * @return the current position of the object
 	 */
-	public Pair<Integer, Integer> getPos(){
-		return this.pos; 
+	public Pair<Integer, Integer> getPos() {
+		return this.pos;
 	}
-	
+
 	/**
 	 * 
 	 * @return the name of the GameObject
 	 */
 	public String getName() {
-		return this.name; 
-	} 
-	
+		return this.name;
+	}
+
 	/**
 	 * 
-	 * @return the object's texture path
+	 * @return the object's texture image
 	 */
-	public String getImagePath() {
-		return this.texturePath;
-	} 
-		
+	@Override
+	public Image getTextureImage() {
+		return this.textureImage;
+	}
+
 	/**
 	 * 
-	 * @param player represent the player to which the effect will be applied
+	 * @param subject represent the subject to which the effect will be applied
 	 */
-	public abstract void execute(Player player); 
-	
+	public abstract void execute(ArtefactUserObject subject);
+
 }
