d0ab025d1e8d1b16794e7f61d208bfe73f9dc3e1
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-05-01 19:00:43 +0200

    create interface for the health artefact

--- src/logics/game_object/artefact/HealthArtefactFactory.java
+++ src/logics/game_object/artefact/HealthArtefactFactory.java
@@ -1,23 +1,25 @@
 package logics.game_object.artefact;
 
-public interface ArtefactFactory {
+import utilis.Pair;
+
+public interface HealthArtefactFactory {
 	
 	/**
 	 * 
-	 * @return an artefact that heal who consume it
+	 * @return an artefact that heal a little who consume it
 	 */
-	public Artefact healArtefact(); 
+	public Artefact littleHealArtefact(Pair<Integer, Integer> pos); 
+	
+	/**
+	 * 
+	 * @return an artefact that heal a lot who consume it
+	 */
+	public Artefact bigHealArtefact(Pair<Integer, Integer> pos); 
 	
 	/**
 	 * 
 	 * @return a life-extending artefact 
 	 */
-	public Artefact extendsLifeArtefact();
+	public Artefact extendsLifeArtefact(Pair<Integer, Integer> pos);
 	
-	
-	/**
-	 * 
-	 * @return a movement-changer artefact; 
-	 */
-	public Artefact changeMovingStrategyArtefact(); 
 }
