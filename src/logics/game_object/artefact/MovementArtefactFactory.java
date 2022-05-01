9bc8c9026e044c90b04433791d73efa0bb8a80fb
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-05-01 19:33:44 +0200

    change javadoc

--- src/logics/game_object/artefact/MovementArtefactFactory.java
+++ src/logics/game_object/artefact/MovementArtefactFactory.java
@@ -6,12 +6,14 @@ public interface MovementArtefactFactory {
 	
 	/**
 	 * 
+	 * @param pos is the artefact spawn position
 	 * @return an artefact that change the subject moving strategy in the Step one
 	 */
 	public Artefact stepArtefact(Pair<Integer, Integer> pos); 
 	
 	/**
-	 * 
+	 *
+	 * @param pos is the artefact spawn position
 	 * @return an artefact that change the subject moving strategy in the Run one
 	 */
 	public Artefact runArtefact(Pair<Integer, Integer> pos);
