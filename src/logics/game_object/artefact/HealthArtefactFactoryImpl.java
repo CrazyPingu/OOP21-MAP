976f26ac396aafdbef14bbc8ce13bd69696c61fd
ElektroDuck <93348024+ElektroDuck@users.noreply.github.com>
2022-05-02 16:40:23 +0200

    Hotfix. Fix the artefacts texture references

--- src/logics/game_object/artefact/HealthArtefactFactoryImpl.java
+++ src/logics/game_object/artefact/HealthArtefactFactoryImpl.java
@@ -7,9 +7,9 @@ import utilis.texture.ArtefactTexture;
 public class HealthArtefactFactoryImpl implements HealthArtefactFactory {
 
 	public final int LITTLE_HEAL_VALUE = 2;
-	public final int BIG_HEAL_VALUE= 4;
+	public final int BIG_HEAL_VALUE = 4;
 	public final int LIFE_EXTENSION_VALUE = 1;
-	
+
 	@Override
 	public Artefact littleHealArtefact(Pair<Integer, Integer> pos) {
 		return new Artefact(pos, "little heal", ArtefactTexture.LITTLE_HEAL) {
@@ -23,18 +23,18 @@ public class HealthArtefactFactoryImpl implements HealthArtefactFactory {
 
 	@Override
 	public Artefact bigHealArtefact(Pair<Integer, Integer> pos) {
-		return new Artefact(pos, "big heal", ArtefactTexture.LITTLE_HEAL) {
+		return new Artefact(pos, "big heal", ArtefactTexture.BIG_HEAL) {
 
 			@Override
 			public void execute(ArtefactUserObject subject) {
-				subject.heal(BIG_HEAL_VALEU);
+				subject.heal(BIG_HEAL_VALUE);
 			}
 		};
 	}
 
 	@Override
 	public Artefact extendsLifeArtefact(Pair<Integer, Integer> pos) {
-		return new Artefact(pos, "life extension", ArtefactTexture.LITTLE_HEAL) {
+		return new Artefact(pos, "life extension", ArtefactTexture.HEALTH_EXTENDER) {
 
 			@Override
 			public void execute(ArtefactUserObject subject) {
