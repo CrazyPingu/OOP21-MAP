212e6ea9423bfda61d639ce6f395938056597aae
Luca Babboni <luca.babboni01@gmail.com>
2022-05-06 18:19:42 +0200

    implement method in the interface ActionNumberFactoryImpl

---
+++ src/logics/game_object/artefact/ActionNumberArtefactFactoryImpl.java
@@ -0,0 +1,20 @@
+package logics.game_object.artefact;
+
+import logics.game_object.ArtefactUserObject;
+import utilis.Pair;
+import utilis.texture.ArtefactTexture;
+
+public class ActionNumberArtefactFactoryImpl implements ActionNumberArteactFactory {
+
+	@Override
+	public Artefact increaseActionNumberArtefact(Pair<Integer, Integer> pos) {
+		return new Artefact(pos, "number action increaser", ArtefactTexture.ACTION_NUMBER_INCREASER) {
+
+			@Override
+			public void execute(ArtefactUserObject subject) {
+				subject.increaseActionNumber();
+			}
+		};
+	}
+
+}
