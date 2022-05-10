f132fa822bd2906cde18bf6fb086e8c1d88db8a4
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-11 01:47:40 +0200

    Fix BigRoom error

--- src/logics/room/type/BigRoom.java
+++ src/logics/room/type/BigRoom.java
@@ -1,8 +1,9 @@
 package logics.room.type;
 
-import logics.room.works.RandomEnemyMap;
+import logics.game_object.entity.Player;
 import logics.room.works.RoomImpl;
 import utilis.Pair;
+
 /**
  * 
  * Class that define the room with the max size
@@ -12,8 +13,8 @@ public class BigRoom extends RoomImpl {
 	/**
 	 * {@inheritDoc}
 	 */
-	public BigRoom(Pair<Integer, Integer> size) {
-		super(size, new Pair<Integer, Integer>(2, size.getY() / 2), new RandomEnemyMap(size));
+	public BigRoom(Pair<Integer, Integer> size, Player player) {
+		super(size, player, new Pair<Integer, Integer>(2, size.getY() / 2));
 	}
 
 }
