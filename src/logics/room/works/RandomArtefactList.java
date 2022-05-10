cced5d1277744c1565657aabf6d72d82e52cca64
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-11 00:57:28 +0200

    Create RandomArtefactList

---
+++ src/logics/room/works/RandomArtefactList.java
@@ -0,0 +1,16 @@
+package logics.room.works;
+
+import java.util.ArrayList;
+
+import logics.game_object.GameObject;
+import utilis.Pair;
+
+public class RandomArtefactList extends ArrayList<GameObject>{
+
+	private static final long serialVersionUID = -6692451133647329784L;
+
+	
+	public RandomArtefactList(Pair<Integer, Integer> size) {
+		
+	}
+}
