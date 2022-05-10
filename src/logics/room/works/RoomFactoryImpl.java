4d252d34970b02933ff8ac7938d0a078dac629e5
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-11 01:50:36 +0200

    Fix RoomFactoryImpl with the change of the last commit

--- src/logics/room/works/RoomFactoryImpl.java
+++ src/logics/room/works/RoomFactoryImpl.java
@@ -1,6 +1,8 @@
 package logics.room.works;
 
 import java.util.Random;
+
+import logics.game_object.entity.Player;
 import logics.room.type.BigRoom;
 import logics.room.type.RandomRoom;
 import logics.room.type.SmallRoom;
@@ -14,6 +16,9 @@ import utilis.Pair;
  */
 public class RoomFactoryImpl implements RoomFactory {
 
+	
+	private Player player;
+	
 	private final int maxX = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 15);
 	private final int maxY = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 10);
 	private final Pair<Integer, Integer> maxSize = new Pair<Integer, Integer>(maxX, maxY);
@@ -22,18 +27,21 @@ public class RoomFactoryImpl implements RoomFactory {
 	private final int minY = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 5);
 	private final Pair<Integer, Integer> minSize = new Pair<Integer, Integer>(minX, minY);
 
+	public RoomFactoryImpl(Player player) {
+		this.player = player;
+	}
 	/**
 	 * {@inheritDoc}
 	 */
 	public Room createBigRoom() {
-		return new BigRoom(maxSize);
+		return new BigRoom(maxSize, player);
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public Room createSmallRoom() {
-		return new SmallRoom(minSize);
+		return new SmallRoom(minSize, player);
 	}
 
 	/**
@@ -43,6 +51,6 @@ public class RoomFactoryImpl implements RoomFactory {
 		Pair<Integer, Integer> randomSize = new Pair<Integer, Integer>(
 				new Random().ints(minSize.getX(), maxSize.getX()).findFirst().getAsInt(),
 				new Random().ints(minSize.getY(), maxSize.getY()).findFirst().getAsInt());
-		return new RandomRoom(randomSize);
+		return new RandomRoom(randomSize, player);
 	}
 }
