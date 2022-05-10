17e0f84910eca407efaaa068405897564f0ee3bc
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-11 01:58:16 +0200

    Make more readable code in RoomFactoryImpl

--- src/logics/room/works/RoomFactoryImpl.java
+++ src/logics/room/works/RoomFactoryImpl.java
@@ -6,8 +6,8 @@ import logics.game_object.entity.Player;
 import logics.room.type.BigRoom;
 import logics.room.type.RandomRoom;
 import logics.room.type.SmallRoom;
-import utilis.Constant;
 import utilis.Pair;
+import utilis.RoomConstant;
 
 /**
  * 
@@ -16,14 +16,14 @@ import utilis.Pair;
  */
 public class RoomFactoryImpl implements RoomFactory {
 
-	
 	private Player player;
-	private final Pair<Integer, Integer> maxSize = new Pair<Integer, Integer>(maxX, maxY);
-	private final Pair<Integer, Integer> minSize = new Pair<Integer, Integer>(minX, minY);
+	private final Pair<Integer, Integer> maxSize = new Pair<Integer, Integer>(RoomConstant.MAX_X, RoomConstant.MAX_X);
+	private final Pair<Integer, Integer> minSize = new Pair<Integer, Integer>(RoomConstant.MIN_X, RoomConstant.MIN_Y);
 
 	public RoomFactoryImpl(Player player) {
 		this.player = player;
 	}
+
 	/**
 	 * {@inheritDoc}
 	 */
--- src/utilis/RoomConstant.java
+++ src/utilis/RoomConstant.java
@@ -5,10 +5,10 @@ public class RoomConstant {
 	public static final int SPAWNING_RATIO = 30;
 	public static final int FORBIDDEN_ZOMBIE_SPAWN = 3;
 	
-	private final int maxX = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 15);
-	private final int maxY = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 10);
+	public static final int MAX_X = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 15);
+	public static final int MAX_Y = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 10);
 
-	private final int minX = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 6);
-	private final int minY = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 5);
+	public static final int MIN_X = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 6);
+	public static final int MIN_Y = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 5);
 	
 }
