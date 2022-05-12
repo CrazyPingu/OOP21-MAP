1a0964d962114542bfcab9b2e8d9c2d1b0f868fb
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-12 23:06:47 +0200

    Fix ImageModifier

--- src/utilities/ImageModifier.java
+++ src/utilities/ImageModifier.java
@@ -1,33 +1,26 @@
 package utilities;
 
-import java.awt.*;
-import javax.swing.JButton;
+import java.awt.Dimension;
+import java.awt.Image;
 
 public class ImageModifier {
 
-	// given the image, and the size of the button / label it will scale based on
-	// your monitor
-	public static final Image scale(Image image, Dimension size) {
-		double scaleFactor = Math.min(1d,
-				getScaleFactorToFit(new Dimension(image.getWidth(null), image.getHeight(null)), size));
-		int scaleWidth = (int) Math.round(image.getWidth(null) * scaleFactor);
-		int scaleHeight = (int) Math.round(image.getHeight(null) * scaleFactor);
-		return image.getScaledInstance(scaleWidth, scaleHeight, Image.SCALE_SMOOTH);
-	}
-
-	private static final double getScaleFactorToFit(Dimension original, Dimension toFit) {
-		double dScale = 1d;
-		if (original != null && toFit != null) {
-			double dScaleWidth = (double) toFit.width / (double) original.width;
-			double dScaleHeight = (double) toFit.height / (double) original.height;
-			dScale = Math.min(dScaleHeight, dScaleWidth);
-		}
-		return dScale;
-	}
-
-	// given an image and the size of the button / label it will fill the
+	/**
+	 * @param img the image that will be scaled
+	 * @return the image scaled with the dimension of your screen
+	 */
 	public static final Image scaleFullScreen(Image img) {
-		return img.getScaledInstance(Constant.WIDTH, Constant.HEIGHT, Image.SCALE_DEFAULT);
+		return scaleWithDimension(img, new Dimension(Constant.WIDTH, Constant.HEIGHT));
+	}
+
+	/**
+	 * 
+	 * @param img the image that will be scaled
+	 * @param dim the dimension your image will be
+	 * @return the image scaled with the dimension you passed
+	 */
+	public static final Image scaleWithDimension(Image img, Dimension dim) {
+		return img.getScaledInstance((int) dim.getWidth(), (int) (dim.getHeight()), Image.SCALE_DEFAULT);
 	}
 
 }
