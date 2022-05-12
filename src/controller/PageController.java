3d456c7d1ac9e8afec1bc1f8364824a3dd5ba3d2
Barto00 <federico.bartolini6@studio.unibo.it>
2022-05-12 20:20:58 +0200

    Create PageController

---
+++ src/controller/PageController.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class PageController {
+    
+    public PageController() {
+        
+    }
+    
+    public void setupAllCardLayout() {
+        
+    }
+    
+    public void showMainMenu() {
+        
+    }
+    
+    public void showLoadingScreen() {
+        
+    }
+
+}
