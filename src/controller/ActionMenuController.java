79bd2f21e2c682472d2d3538e6652719977c6757
Barto00 <federico.bartolini6@studio.unibo.it>
2022-05-12 22:26:05 +0200

    Create ActionMenuController

---
+++ src/controller/ActionMenuController.java
@@ -0,0 +1,33 @@
+package controller;
+
+public class ActionMenuController {
+
+    public ActionMenuController() {
+        
+    }
+    
+    public void  setInitialActionNumber () {
+        
+    }
+    
+    public void decreaseAction() {
+        
+    }
+    
+    public void attack() {
+        
+    }
+    
+    public void move() {
+        
+    }
+    
+    public void skip() {
+        
+    }
+    
+    public void setTypeOfActionFlag() {
+        
+    }
+    
+}
