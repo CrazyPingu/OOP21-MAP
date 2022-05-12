ee7032806a94156b3b0805c43e17c9cd05ed3ad8
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-12 22:13:58 +0200

    Create basic game controller

---
+++ src/controller/gameController/BasicGameController.java
@@ -0,0 +1,17 @@
+package controller.gameController;
+
+public class BasicGameController extends GameController {
+
+	@Override
+	public void playerTurn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enemyTurn() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
