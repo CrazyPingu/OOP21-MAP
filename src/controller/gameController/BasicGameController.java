ea99e75dce09826e109b2df1c9235ad89ffcbe53
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-12 22:56:13 +0200

    add javadoc and parameter to playerTurn

--- src/controller/gameController/BasicGameController.java
+++ src/controller/gameController/BasicGameController.java
@@ -1,17 +1,29 @@
 package controller.gameController;
 
+/**
+ * 
+ * Concrete class that implements a basic type of game.
+ *
+ */
 public class BasicGameController extends GameController {
 
+	/**
+	 * {@inheritDoc}
+	 * @param actionMenuController: is the action manager for the player turn
+	 */
 	@Override
-	public void playerTurn() {
+	public void playerTurn(ActionMenuController actionMenuController) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void enemyTurn() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
