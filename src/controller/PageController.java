e4ffb886ca9066950c7c856abbbd91aed9bc7e50
Barto00 <federico.bartolini6@studio.unibo.it>
2022-05-12 21:04:50 +0200

    Update PageController

--- src/controller/PageController.java
+++ src/controller/PageController.java
@@ -1,37 +1,60 @@
 package controller;
 
+import logics.room.works.Room;
+import view.frame.BasicFrame;
+import view.game.TotalPanel;
+import view.loadingScreen.LoadingScreenImpl;
+import view.pause.PauseMenu;
+
 public class PageController {
     
-    public PageController() {
-        
+    BasicFrame frame;
+    Room room;
+    
+    public PageController(BasicFrame frame) {
+        this.frame = frame;
     }
     
     public void setupAllCardLayout() {
+        MainMenu mm = new MainMenu();
+        PauseMenu pm = new PauseMenu(frame);
+        TotalPanel tp = new TotalPanel(room);
+        LoadingScreenImpl ls = new LoadingScreenImpl();
+        LoseGame lg = new LoseGame();
+        WinGame wg = new WinGame();
+        
+        
+        frame.addToCardLayout(mm, "MainMenu");
+        frame.addToCardLayout(pm, "PauseMenu");
+        frame.addToCardLayout(tp, "Game");
+        frame.addToCardLayout(ls, "LoadingScreen");
+        frame.addToCardLayout(lg, "Defeat");
+        frame.addToCardLayout(wg, "Win");
         
     }
     
     public void showMainMenu() {
-        
+        frame.showInFrame("MainMenu");
     }
     
     public void showLoadingScreen() {
-        
+        frame.showInFrame("LoadingScreen");
     }
     
     public void showGame() {
-        
+        frame.showInFrame("Game");
     }
     
     public void showPauseMenu() {
-        
+        frame.showInFrame("PauseMenu");
     }
     
     public void showLostGame() {
-        
+        frame.showInFrame("Defeat");
     }
     
     public void showWinGame() {
-        
+        frame.showInFrame("Win");
     }
 
 }
