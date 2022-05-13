02a8600eb2ee776af2bab41542d05e1e36b646b5
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-13 22:50:20 +0200

    Changed value in constant

--- src/utilities/RoomConstant.java
+++ src/utilities/RoomConstant.java
@@ -12,11 +12,11 @@ public class RoomConstant {
 	public static final int SPAWNING_RATIO = 30;
 	public static final int FORBIDDEN_ZOMBIE_SPAWN = 3;
 
-	public static final int MAX_X = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 15);
+	public static final int MAX_X = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 20);
 	public static final int MAX_Y = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 10);
 
-	public static final int MIN_X = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 6);
-	public static final int MIN_Y = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 5);
+	public static final int MIN_X = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 12);
+	public static final int MIN_Y = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 7);
 
 	/**
 	 * @param pos            the position of the GameObject to be found
