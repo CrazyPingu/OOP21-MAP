3cc0382d9293e5bbbcc85ed07e2523f0f96a05b1
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-14 11:11:51 +0200

    add javadoc

--- src/controller/enemyAI/EnemyAI.java
+++ src/controller/enemyAI/EnemyAI.java
@@ -8,17 +8,17 @@ package controller.enemyAI;
 public interface EnemyAI {
 	
 	/**
-	 * 
+	 * enemy automated movement
 	 */
 	public void move();
 	
 	/**
-	 * 
+	 * enemy automated attack
 	 */
     public void attack();
     
     /**
-     * 
+     * checks if player is reachable by the enemy attack
      */
     public boolean isPlayerInAttackArea();
 
