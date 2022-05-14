e385ce0bccd39f4c0e7998718920a4f4a4800ebb
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-14 14:44:14 +0200

    Add constant to RoomConstant

--- src/utilities/RoomConstant.java
+++ src/utilities/RoomConstant.java
@@ -2,6 +2,8 @@ package utilities;
 
 import java.util.List;
 
+import javax.swing.ImageIcon;
+
 import logics.game_object.GameObject;
 import logics.game_object.artefact.Artefact;
 import logics.game_object.entity.Player;
@@ -17,6 +19,9 @@ public class RoomConstant {
 
 	public static final int MIN_X = Constant.GAME_WIDTH / Constant.horizontalAspectRatio(Constant.GAME_WIDTH / 12);
 	public static final int MIN_Y = Constant.TOP_HEIGHT / Constant.verticalAspectRatio(Constant.TOP_HEIGHT / 7);
+
+	public static final ImageIcon ATTACK_HIGHLIGHT = ImageMethod.getImageIcon("room/RedBall.png");
+	public static final ImageIcon MOVE_HIGHLIGHT = ImageMethod.getImageIcon("room/BlueBall.png");
 
 	/**
 	 * @param pos            the position of the GameObject to be found
