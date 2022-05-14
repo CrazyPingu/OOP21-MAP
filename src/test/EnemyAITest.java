620e6911d48944d428b6b049bd0bdab4211c208f
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-14 18:27:13 +0200

    Implement moving test with player in different coordinates than area's cells

--- src/test/EnemyAITest.java
+++ src/test/EnemyAITest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import controller.enemyAI.EnemyAIImpl;
 import logics.game_object.entity.Player;
 import logics.game_object.entity.SimpleEnemy;
 import logics.game_object.entity.enemy.EnemyFactoryImpl;
 import logics.life.ExtendibleMaxLifeSystem;
-import logics.room.works.RoomImpl;
 import logics.strategy.movement.MovementFactoryImpl;
 import logics.strategy.weapon.WeaponFactoryImpl;
 import utilities.Pair;
@@ -21,23 +21,23 @@ import utilities.texture.EntityTexture;
  */
 
 public class EnemyAITest {
-	WeaponFactoryImpl wf;
-    MovementFactoryImpl mf;
-    Player player;
-    RoomImpl room;
-    EnemyFactoryImpl ef;
-    SimpleEnemy enemy;
-    List<Pair<Integer,Integer>> expectedResults;
+	private EnemyAIImpl enemyAI;
+	private WeaponFactoryImpl wf;
+    private MovementFactoryImpl mf;
+    private Player player;
+    private EnemyFactoryImpl ef;
+    private SimpleEnemy enemy;
+    private Pair<Integer,Integer> expectedResult;
+    private Pair<Integer,Integer> roomSize = new Pair<> (10, 4);
     
     @org.junit.Before
 	public void init() {
+    	this.enemyAI = new EnemyAIImpl();
     	this.wf = new WeaponFactoryImpl();
     	this.mf = new MovementFactoryImpl();
     	this.player = new Player(new ExtendibleMaxLifeSystem(4, 10, 20), new Pair<>(2,3), wf.createAxe(),
                 mf.stepMovement(), "Marcello", EntityTexture.PLAYER);
-		this.room = new RoomImpl(new Pair<> (10, 4), player,  new Pair<>(2,3));
-		this.enemy = ef.createZombieStick(new Pair<> (1,5));
-		this.expectedResults.clear();
+		this.enemy = ef.createZombieStick(new Pair<> (5,1));
 	}
 	
 	@org.junit.Test
@@ -45,6 +45,12 @@ public class EnemyAITest {
 	 * testing of enemy's moving area with different coordinates than player's position
 	 */
 	public void playerOutside() {
+		this.player.setPos(new Pair<>(2,3));
 		
+		this.expectedResult = new Pair<>(4,2);
+		this.enemyAI.move(enemy, player, roomSize, null);
+		assertEquals(expectedResult, this.enemy.getPos());
 	}
+	
+	
 }
