06d96842cfca481900367d4e27cbe62f17d63c01
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-17 04:51:00 +0200

    add @param in javadoc

--- src/controller/enemyAI/EnemyAI.java
+++ src/controller/enemyAI/EnemyAI.java
@@ -13,16 +13,22 @@ public interface EnemyAI {
 	
 	/**
 	 * enemy automated movement
+	 * @param enemy: enemy of which is needed to change position
 	 */
 	public void move(SimpleEnemy enemy);
 	
 	/**
 	 * enemy automated attack
+	 * @param enemy: makes damage
+	 * @param player: takes in the damage
 	 */
     public void attack(SimpleEnemy enemy, Player player);
     
     /**
      * checks if player is reachable by the enemy attack
+     * @param enemy: entity of which is needed to know the attackable area
+     * @param player: entity to be found whether or not is in the attackable area
+     * @param roomSize: size of the current room
      */
     public boolean isPlayerInAttackArea(SimpleEnemy enemy, Player player, Pair<Integer,Integer> roomSize);
 
--- src/controller/gameController/BasicGameController.java
+++ src/controller/gameController/BasicGameController.java
@@ -24,6 +24,7 @@ public class BasicGameController extends GameController {
 	
 	/**
 	 * {@inheritDoc}
+	 * @param actionMenuController: controller of the action menu
 	 */
 	@Override
 	public void playerTurn(ActionMenuController actionMenuController) {
