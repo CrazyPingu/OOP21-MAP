e0de81b8b62ce979a87226d4a641df14d09553c8
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-05-17 14:41:30 +0200

    Hotfix actionMenu

--- src/view/MenuButton.java
+++ src/view/MenuButton.java
@@ -10,13 +10,12 @@ public class MenuButton extends JButton {
 
     private static final long serialVersionUID = -8929834616886633143L;
 
-    public MenuButton(String text) {
+    public MenuButton(String text, int font) {
         super(text);
-        this.setFont(Constant.genericFont("Tahoma", Font.PLAIN, Constant.horizontalAspectRatio(40)));
+        this.setFont(Constant.genericFont("Tahoma", Font.PLAIN, Constant.horizontalAspectRatio(font)));
         this.setBackground(new Color(197, 199, 196));
         this.setBorder(
                 BorderFactory.createLineBorder(new Color(67, 59, 103), Constant.horizontalAspectRatio(10), false));
-
     }
 
 }
