11e7be3b73b98870cf31b927a08219fc75460667
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-05-19 11:19:20 +0200

    Create endGameMenu

---
+++ src/view/endGameMenu/EndGameMenu.java
@@ -0,0 +1,12 @@
+package view.endGameMenu;
+
+import view.MenuButton;
+
+public class EndGameMenu {
+
+    MenuButton mainMenu = new MenuButton("Main Menu", 40);
+    MenuButton quit = new MenuButton("Quit", 40);
+    
+    
+    
+}
