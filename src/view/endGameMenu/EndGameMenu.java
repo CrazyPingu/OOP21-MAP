f63f820f283e481f6d6f120bdc7a58910b3c1e93
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-05-19 11:51:04 +0200

    Delete useless images and Update EndGameMenu

--- src/view/endGameMenu/EndGameMenu.java
+++ src/view/endGameMenu/EndGameMenu.java
@@ -1,11 +1,51 @@
 package view.endGameMenu;
 
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import controller.PageController;
+import utilities.Constant;
+import utilities.GbcDimension;
+import utilities.ImageMethod;
+import utilities.ImageModifier;
 import view.MenuButton;
+import view.pause.action.*;
 
-public class EndGameMenu {
+public class EndGameMenu extends JPanel{
 
+    private static final long serialVersionUID = 6789169156087285519L;
+    PageController controller;
     MenuButton mainMenu = new MenuButton("Main Menu", 40);
     MenuButton quit = new MenuButton("Quit", 40);
+    QuitAction quitAction = new QuitAction(controller);
+    MainMenuAction mainMenuAction = new MainMenuAction(controller);
+    Image image;
+    
+    public EndGameMenu(String imageName) {
+        
+        this.image = ImageMethod.getImage("endGame/" + imageName + ".png");
+        quit.addActionListener(quitAction);
+        mainMenu.addActionListener(mainMenuAction);
+        this.setLayout(new BorderLayout());
+
+        this.add(mainMenu, new GbcDimension(0, 0, Constant.horizontalAspectRatio(300), Constant.verticalAspectRatio(150),
+                GbcDimension.createInsets(0, 0, 0, 100)));
+        this.add(quit, new GbcDimension(1, 0, Constant.horizontalAspectRatio(300), Constant.verticalAspectRatio(150),
+                GbcDimension.createInsets(0, 100, 0, 0)));
+        this.add(this, BorderLayout.SOUTH);
+    }
+    
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(ImageModifier.scaleFullScreen(this.image), 0, 0, null);
+    }
+    
     
     
     
