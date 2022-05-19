f0ab0868630b087d9ba2bc135c870aefa708f6c0
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-05-19 13:34:24 +0200

    Create defeat and victory screen

---
+++ src/view/endGameMenu/Defeat.java
@@ -0,0 +1,13 @@
+package view.endGameMenu;
+
+import controller.PageController;
+
+public class Defeat extends EndGameMenu {
+
+    private static final long serialVersionUID = 3843742944194787112L;
+
+    public Defeat(String imageName, PageController controller) {
+        super(imageName, controller);
+    }
+
+}
