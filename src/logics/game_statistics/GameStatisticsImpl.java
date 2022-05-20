7ab22b3439fbbed47e1bd1ef2508520a3ca9322a
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-20 10:58:55 +0200

    add javadoc

--- src/logics/game_statistics/GameStatisticsImpl.java
+++ src/logics/game_statistics/GameStatisticsImpl.java
@@ -7,26 +7,32 @@ public class GameStatisticsImpl implements GameStatistics {
 	private CounterImpl passedRooms;
 	private CounterImpl collectedArtefacts;
 
+	@Override
 	public void increaseKilledEnemies() {
 		killedEnemies.increment();
 	}
 
+	@Override
 	public void increasePassedRooms() {
 		passedRooms.increment();
 	}
 	
+	@Override
 	public void increaseCollectedArtefacts() {
 		passedRooms.increment();
 	}
 
+	@Override
 	public CounterImpl getKilledEnemies() {
 		return killedEnemies;
 	}
 
+	@Override
 	public CounterImpl getPassedRoom() {
 		return passedRooms;
 	}
 	
+	@Override
 	public CounterImpl getCollectedArtefacts() {
 		return collectedArtefacts;
 	}
