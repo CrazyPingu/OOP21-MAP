2d6327ffb890a38e16466e83e290c50962f45bea
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-05-20 17:02:12 +0200

    Fix Buttons dimension

--- src/view/endGameMenu/EndGameMenu.java
+++ src/view/endGameMenu/EndGameMenu.java
@@ -1,6 +1,7 @@
 package view.endGameMenu;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.Image;
@@ -18,21 +19,26 @@ public abstract class EndGameMenu extends JPanel {
 
     private static final long serialVersionUID = 6789169156087285519L;
     Image image;
+    private final int BUTTON_WIDTH =300;
+    private final int BUTTON_HEIGHT = 100;
 
     public EndGameMenu(String imageName, PageController controller) {
 
-        MenuButton mainMenu = new MenuButton("Main Menu", 40);
-        MenuButton quit = new MenuButton("Quit", 40);
+        MenuButton mainMenu = new MenuButton("Main Menu", 60);
+        MenuButton quit = new MenuButton("Quit", 60);
 
         this.image = ImageMethod.getImage("endGame/" + imageName + ".png");
         quit.addActionListener(new QuitAction(controller));
         mainMenu.addActionListener(new MainMenuAction(controller));
         this.setLayout(new BorderLayout());
+
+        quit.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+        mainMenu.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+
         JPanel bottom = new JPanel(new GridBagLayout());
-        bottom.add(mainMenu, new GbcDimension(0, 0, Constant.horizontalAspectRatio(300),
-                Constant.verticalAspectRatio(150), GbcDimension.createInsets(0, 0, 0, 100)));
-        bottom.add(quit, new GbcDimension(1, 0, Constant.horizontalAspectRatio(300), Constant.verticalAspectRatio(150),
-                GbcDimension.createInsets(0, 100, 0, 0)));
+        bottom.add(mainMenu,
+                new GbcDimension(0, 0, BUTTON_WIDTH, BUTTON_HEIGHT, GbcDimension.createInsets(0, 0, 0, 100)));
+        bottom.add(quit, new GbcDimension(1, 0, BUTTON_WIDTH, BUTTON_HEIGHT, GbcDimension.createInsets(0, 100, 0, 0)));
         this.add(bottom, BorderLayout.SOUTH);
         bottom.setOpaque(false);
     }
