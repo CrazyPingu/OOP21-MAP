d85706cbe5c0c551d27d7eeb9540e274a58899fb
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-22 15:27:40 +0200

    Fix LoadingScreen interface

--- src/view/loadingScreen/LoadingScreen.java
+++ src/view/loadingScreen/LoadingScreen.java
@@ -1,6 +1,5 @@
 package view.loadingScreen;
 
-
 /**
  * 
  * Interface to generate a LoadingScreen
@@ -8,30 +7,30 @@ package view.loadingScreen;
  */
 public interface LoadingScreen {
 
-	/**
-	 * @param name : the file name of the image
-	 */
-	public void addImage(String name);
+  /**
+   * @param name : the file name of the image
+   */
+  void addImage(String name);
 
-	/**
-	 * Add the title "Marcello Apocalypse"
-	 */
-	public void addText();
+  /**
+   * Add the title "Marcello Apocalypse"
+   */
+  void addText();
 
-	/**
-	 * Method to add a progress bar
-	 */
-	public void addProgressBar();
+  /**
+   * Method to add a progress bar
+   */
+  void addProgressBar();
 
-	/**
-	 * Method to add in the JPanel a JLabel that display the percentage value of the
-	 * progress bar
-	 */
-	public void addMessage();
+  /**
+   * Method to add in the JPanel a JLabel that display the percentage value of the
+   * progress bar
+   */
+  void addMessage();
 
-	/**
-	 * Start the progress bar and show the game at the end of it
-	 */
-	public void startProgressBar();
+  /**
+   * Start the progress bar and show the game at the end of it
+   */
+  void startProgressBar();
 
 }
