1a41195ac73fe75bc9c7e25eae514c2ce506ed6c
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-22 14:48:23 +0200

    add final to fields

--- src/controller/gameController/BasicGameController.java
+++ src/controller/gameController/BasicGameController.java
@@ -18,51 +18,52 @@ import view.game.TotalPanel;
  */
 public class BasicGameController extends GameController {
 
-	private EnemyAIImpl enemyAI;
-	private final int ROOM_TO_WIN = 3;
+    final private EnemyAIImpl enemyAI;
+    private static final int ROOM_TO_WIN = 3;
 
-	public BasicGameController(GameAreaController gameAreaController, TotalPanel totalPanel,
-			PageController pageController, GameStatisticsImpl gameStats) {
-		super(gameAreaController, totalPanel, pageController, gameStats);
-		this.enemyAI = new EnemyAIImpl(this.getTotalPanel());
-	}
+    public BasicGameController(final GameAreaController gameAreaController, final TotalPanel totalPanel,
+            final PageController pageController, final GameStatisticsImpl gameStats) {
+        super(gameAreaController, totalPanel, pageController, gameStats);
+        this.enemyAI = new EnemyAIImpl(this.getTotalPanel());
+    }
 
-	@Override
-	public void enemyTurn() {
-		List<SimpleEnemy> allEnemyList = this.getTotalPanel().getGameArea().getRoom().getEnemyList();
-		Player player = this.getTotalPanel().getGameArea().getRoom().getPlayer();
-		Pair<Integer, Integer> roomSize = this.getTotalPanel().getGameArea().getRoom().getSize();
+    @Override
+    public void enemyTurn() {
+        final List<SimpleEnemy> allEnemyList = this.getTotalPanel().getGameArea().getRoom().getEnemyList();
+        final Player player = this.getTotalPanel().getGameArea().getRoom().getPlayer();
+        final Pair<Integer, Integer> roomSize = this.getTotalPanel().getGameArea().getRoom().getSize();
 
-		for (SimpleEnemy enemy : allEnemyList) {
-			if (this.enemyAI.isPlayerInAttackArea(enemy, player, roomSize)) {
-				player.damage(enemy.getWeapon().getDamage());
-				if (player.isDead()) {
-					this.getPageController().showDefeat();
-				} else {
-					this.getTotalPanel().getScrollableStats().getStatsValues().update(player, this.getGameStats(), this.getCurrentActionNumber());
-					this.getTotalPanel().getScrollableLog().getLogMessage().update("" + player.getName() + " got hit "
-							+ enemy.getWeapon().getDamage() + " by " + enemy.getName() + "!");
-				}
-			} else {
-				this.getTotalPanel().getGameArea().moveGameObject(enemy.getPos(), this.enemyAI.move(enemy));
-			}
-		}
-		this.resetActionNumber();
-	}
+        for (final SimpleEnemy enemy : allEnemyList) {
+            if (this.enemyAI.isPlayerInAttackArea(enemy, player, roomSize)) {
+                player.damage(enemy.getWeapon().getDamage());
+                if (player.isDead()) {
+                    this.getPageController().showDefeat();
+                } else {
+                    this.getTotalPanel().getScrollableStats().getStatsValues().update(player, this.getGameStats(),
+                            this.getCurrentActionNumber());
+                    this.getTotalPanel().getScrollableLog().getLogMessage().update(player.getName() + " got hit "
+                            + enemy.getWeapon().getDamage() + " by " + enemy.getName() + "!");
+                }
+            } else {
+                this.getTotalPanel().getGameArea().moveGameObject(enemy.getPos(), this.enemyAI.move(enemy));
+            }
+        }
+        this.resetActionNumber();
+    }
 
-	@Override
-	public boolean isDoorAvailable() {
-		return true;
-	}
+    @Override
+    public boolean isDoorAvailable() {
+        return true;
+    }
 
-	@Override
-	public boolean isWon() {
-		return this.getGameStats().getCompletedRooms() == ROOM_TO_WIN;
-	}
+    @Override
+    public boolean isWon() {
+        return this.getGameStats().getCompletedRooms() == ROOM_TO_WIN;
+    }
 
-	@Override
-	public boolean isDefeated() {
-		return this.getPlayer().isDead();
-	}
+    @Override
+    public boolean isDefeated() {
+        return this.getPlayer().isDead();
+    }
 
 }
