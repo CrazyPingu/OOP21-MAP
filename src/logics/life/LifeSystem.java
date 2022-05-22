c8bdf381c69877326bb27742b381a36d94f22d60
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-05-22 17:59:54 +0200

    Deleted public

--- src/logics/life/LifeSystem.java
+++ src/logics/life/LifeSystem.java
@@ -11,18 +11,18 @@ public interface LifeSystem {
 	 * 
 	 * @return the current health
 	 */
-	public int getCurrentHealth();
+	int getCurrentHealth();
 
 	/**
 	 * 
 	 * @param damageValue represent the value to decrese from the current health.
 	 *                    Don't work with negative values
 	 */
-	public void damage(int damageValue);
+	void damage(int damageValue);
 
 	/**
 	 * 
 	 * @return the life state of the entity (True = DEAD, FALSE = IN LIFE)
 	 */
-	public Boolean isDead();
+	Boolean isDead();
 }
