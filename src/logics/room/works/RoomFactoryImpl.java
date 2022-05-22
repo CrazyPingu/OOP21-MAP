146d13d0d9168820e54b99c2c7a7884215a9a7ac
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-22 15:08:38 +0200

    Fix roomFactoryImpl

--- src/logics/room/works/RoomFactoryImpl.java
+++ src/logics/room/works/RoomFactoryImpl.java
@@ -16,35 +16,29 @@ import utilities.RoomConstant;
  */
 public class RoomFactoryImpl implements RoomFactory {
 
-	private Player player;
-	private final Pair<Integer, Integer> maxSize = new Pair<Integer, Integer>(RoomConstant.MAX_X, RoomConstant.MAX_Y);
-	private final Pair<Integer, Integer> minSize = new Pair<Integer, Integer>(RoomConstant.MIN_X, RoomConstant.MIN_Y);
+  private final Player player;
+  private final Pair<Integer, Integer> maxSize = new Pair<>(RoomConstant.MAX_X, RoomConstant.MAX_Y);
+  private final Pair<Integer, Integer> minSize = new Pair<>(RoomConstant.MIN_X, RoomConstant.MIN_Y);
 
-	public RoomFactoryImpl(Player player) {
-		this.player = player;
-	}
+  public RoomFactoryImpl(final Player player) {
+    this.player = player;
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public Room createBigRoom() {
-		return new BigRoom(maxSize, player);
-	}
+  @Override
+  public Room createBigRoom() {
+    return new BigRoom(maxSize, player);
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public Room createSmallRoom() {
-		return new SmallRoom(minSize, player);
-	}
+  @Override
+  public Room createSmallRoom() {
+    return new SmallRoom(minSize, player);
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public Room createRandomRoom() {
-		Pair<Integer, Integer> randomSize = new Pair<Integer, Integer>(
-				new Random().ints(minSize.getX(), maxSize.getX()).findFirst().getAsInt(),
-				new Random().ints(minSize.getY(), maxSize.getY()).findFirst().getAsInt());
-		return new RandomRoom(randomSize, player);
-	}
+  @Override
+  public Room createRandomRoom() {
+    return new RandomRoom(
+        new Pair<Integer, Integer>(new Random().ints(minSize.getX(), maxSize.getX()).findFirst().getAsInt(),
+            new Random().ints(minSize.getY(), maxSize.getY()).findFirst().getAsInt()),
+        player);
+  }
 }
