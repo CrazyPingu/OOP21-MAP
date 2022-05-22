e0abbb0185072e9c956b0ea33cf6cab29da4d6f7
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-22 15:04:32 +0200

    add final to fields

--- src/controller/enemy_ai/EnemyAI.java
+++ src/controller/enemy_ai/EnemyAI.java
@@ -11,21 +11,21 @@ import utilities.Pair;
  */
 public interface EnemyAI {
 
-	/**
-	 * enemy automated movement
-	 * 
-	 * @param enemy: enemy of which is needed to change position
-	 * @return the new calculated position for the enemy
-	 */
-	public Pair<Integer, Integer> move(SimpleEnemy enemy);
+    /**
+     * Enemy automated movement.
+     * 
+     * @param enemy: enemy of which is needed to change position
+     * @return the new calculated position for the enemy
+     */
+    Pair<Integer, Integer> move(SimpleEnemy enemy);
 
-	/**
-	 * checks if player is reachable by the enemy attack
-	 * 
-	 * @param enemy:    entity of which is needed to know the attackable area
-	 * @param player:   entity to be found whether or not is in the attackable area
-	 * @param roomSize: size of the current room
-	 */
-	public boolean isPlayerInAttackArea(SimpleEnemy enemy, Player player, Pair<Integer, Integer> roomSize);
+    /**
+     * Checks if player is reachable by the enemy attack.
+     * 
+     * @param enemy:    entity of which is needed to know the attackable area
+     * @param player:   entity to be found whether or not is in the attackable area
+     * @param roomSize: size of the current room
+     */
+    boolean isPlayerInAttackArea(SimpleEnemy enemy, Player player, Pair<Integer, Integer> roomSize);
 
 }
--- src/controller/enemy_ai/EnemyAIImpl.java
+++ src/controller/enemy_ai/EnemyAIImpl.java
@@ -15,42 +15,36 @@ import view.game.TotalPanel;
  */
 public class EnemyAIImpl implements EnemyAI {
 
-	private TotalPanel totalPanel;
+    final private TotalPanel totalPanel;
 
-	public EnemyAIImpl(TotalPanel totalPanel) {
-		this.totalPanel = totalPanel;
-	}
+    public EnemyAIImpl(final TotalPanel totalPanel) {
+        this.totalPanel = totalPanel;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public Pair<Integer, Integer> move(SimpleEnemy enemy) {
-		final List<Pair<Integer, Integer>> enemyReachableArea = enemy.getMovementSystem().reachableCells(enemy.getPos(),
-				this.totalPanel.getGameArea().getRoom().getSize());
-		final Player player = this.totalPanel.getGameArea().getRoom().getPlayer();
-		Pair<Integer, Integer> newEnemyPos = enemy.getPos();
-		int minDistance = Integer.MAX_VALUE;
-		int distance = -1;
+    @Override
+    public Pair<Integer, Integer> move(final SimpleEnemy enemy) {
+        final List<Pair<Integer, Integer>> enemyReachableArea = enemy.getMovementSystem().reachableCells(enemy.getPos(),
+                this.totalPanel.getGameArea().getRoom().getSize());
+        final Player player = this.totalPanel.getGameArea().getRoom().getPlayer();
+        Pair<Integer, Integer> newEnemyPos = enemy.getPos();
+        int minDistance = Integer.MAX_VALUE;
+        int distance;
+        for (final Pair<Integer, Integer> cell : enemyReachableArea) {
+            distance = Math.abs(cell.getX() - player.getPos().getX()) + Math.abs(cell.getY() - player.getPos().getY());
+            if (distance < minDistance
+                    && !(RoomConstant.cellsOccupated(this.totalPanel.getGameArea().getRoom().getEnemyList(),
+                            this.totalPanel.getGameArea().getRoom().getArtefactList(), player, cell))) {
+                newEnemyPos = cell;
+                minDistance = distance;
+            }
+        }
+        return newEnemyPos;
+    }
 
-		for (Pair<Integer, Integer> cell : enemyReachableArea) {
-			distance = Math.abs(cell.getX() - player.getPos().getX()) + Math.abs(cell.getY() - player.getPos().getY());
-			if (distance < minDistance
-					&& !(RoomConstant.cellsOccupated(this.totalPanel.getGameArea().getRoom().getEnemyList(),
-							this.totalPanel.getGameArea().getRoom().getArtefactList(), player, cell))) {
-				newEnemyPos = cell;
-				minDistance = distance;
-			}
-		}
-
-		return newEnemyPos;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public boolean isPlayerInAttackArea(SimpleEnemy enemy, Player player, Pair<Integer, Integer> roomSize) {
-		final List<Pair<Integer, Integer>> attackableArea = enemy.getWeapon().getAttackArea(enemy.getPos(), roomSize);
-		return (attackableArea.contains(player.getPos()));
-	}
+    @Override
+    public boolean isPlayerInAttackArea(final SimpleEnemy enemy, final Player player, final Pair<Integer, Integer> roomSize) {
+        final List<Pair<Integer, Integer>> attackableArea = enemy.getWeapon().getAttackArea(enemy.getPos(), roomSize);
+        return attackableArea.contains(player.getPos());
+    }
 
 }
