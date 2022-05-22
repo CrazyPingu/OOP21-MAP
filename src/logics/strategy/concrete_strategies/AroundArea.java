097163da22a0d49e6dea791517d5f58ef42fd23b
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-05-22 17:32:31 +0200

    indentation and bugfix

--- src/logics/strategy/concrete_strategies/AroundArea.java
+++ src/logics/strategy/concrete_strategies/AroundArea.java
@@ -14,31 +14,31 @@ import utilities.PosInGrid;
  *
  */
 public class AroundArea implements Strategy {
-	
-	private int distance;
 
-	/**
-	 * @param distance is how far from around area can reach
-	 */
-	public AroundArea(int distance) {
-		this.distance = distance;
-	}
+  private final int distance;
 
-	@Override
-	/**
-	 * {inheritDoc}
-	 */
-	public List<Pair<Integer, Integer>> execute(Pair<Integer, Integer> pos, Pair<Integer, Integer> size) {
-		List<Pair<Integer, Integer>> reachableCells = new ArrayList<Pair<Integer, Integer>>();
-		for (int i = pos.getX() - this.distance; i <= pos.getX() + this.distance; i++) {
-			for (int j = pos.getY() - this.distance; j <= pos.getY() + this.distance; j++) {
-				Pair<Integer, Integer> cellToAdd = new Pair<>(i, j);
-				if (!pos.equals(cellToAdd) && PosInGrid.checkPosInGrid(cellToAdd, size)) {
-					reachableCells.add(cellToAdd);
-				}
-			}
-		}
-		return reachableCells;
-	}
+  /**
+   * @param distance is how far from around area can reach
+   */
+  public AroundArea(final int distance) {
+    this.distance = distance;
+  }
+
+  @Override
+  /**
+   * {inheritDoc}
+   */
+  public List<Pair<Integer, Integer>> execute(final Pair<Integer, Integer> pos, final Pair<Integer, Integer> size) {
+    final List<Pair<Integer, Integer>> reachableCells = new ArrayList<>();
+    for (int i = pos.getX() - this.distance; i <= pos.getX() + this.distance; i++) {
+      for (int j = pos.getY() - this.distance; j <= pos.getY() + this.distance; j++) {
+        final Pair<Integer, Integer> cellToAdd = new Pair<>(i, j);
+        if (!pos.equals(cellToAdd) && PosInGrid.checkPosInGrid(cellToAdd, size)) {
+          reachableCells.add(cellToAdd);
+        }
+      }
+    }
+    return reachableCells;
+  }
 
 }
