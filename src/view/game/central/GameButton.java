1bbd0fd8cc846debc6d1a5d7a092cff16b59c8f1
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-22 16:17:39 +0200

    Fix GameButton

--- src/view/game/central/GameButton.java
+++ src/view/game/central/GameButton.java
@@ -22,83 +22,85 @@ import utilities.RoomConstant;
  */
 public class GameButton extends JButton implements ActionListener {
 
-	private static final long serialVersionUID = 1384166202851332499L;
-	private Dimension buttonDimension;
-	private GameAreaController gameAreaController;
-	private Pair<Integer, Integer> pos;
-	private Image image;
-	private Color color;
-	private boolean isDoor;
+  private static final long serialVersionUID = 1384166202851332499L;
+  private final Dimension buttonDimension;
+  private final GameAreaController gameAreaController;
+  private final Pair<Integer, Integer> pos;
+  private Image image;
+  private Color color;
+  private final boolean isDoor;
 
-	public GameButton(Dimension buttonDimension, GameAreaController gameAreaController, Pair<Integer, Integer> pos, boolean isDoor) {
-		this.isDoor = isDoor;
-		this.color = RoomConstant.BASIC_CELL_COLOR;
-		this.pos = pos;
-		image = null;
-		this.setContentAreaFilled(false);
-		this.gameAreaController = gameAreaController;
-		this.buttonDimension = buttonDimension;
-		this.setEnabled(false);
-		this.addActionListener(this);
-		this.setOpaque(true);
-	}
+  public GameButton(final Dimension buttonDimension, final GameAreaController gameAreaController,
+      final Pair<Integer, Integer> pos, final boolean isDoor) {
+    this.isDoor = isDoor;
+    this.color = RoomConstant.BASIC_CELL_COLOR;
+    this.pos = pos;
+    image = null;
+    this.setContentAreaFilled(false);
+    this.gameAreaController = gameAreaController;
+    this.buttonDimension = buttonDimension;
+    this.setEnabled(false);
+    this.addActionListener(this);
+    this.setOpaque(true);
+  }
 
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		if (isDoor) {
-			g.setColor(RoomConstant.DOOR_CELL_COLOR);
-			g.fillRect(0, 0, (int) buttonDimension.getWidth(), (int) buttonDimension.getHeight());
-		} else {
-			g.drawImage(RoomConstant.BUTTON_IMAGE, 0, 0, (int) buttonDimension.getWidth(),
-					(int) buttonDimension.getHeight(), null);
-		}
+  protected void paintComponent(final Graphics g) {
+    super.paintComponent(g);
+    if (isDoor) {
+      g.setColor(RoomConstant.DOOR_CELL_COLOR);
+      g.fillRect(0, 0, (int) buttonDimension.getWidth(), (int) buttonDimension.getHeight());
+    } else {
+      g.drawImage(RoomConstant.BUTTON_IMAGE, 0, 0, (int) buttonDimension.getWidth(), (int) buttonDimension.getHeight(),
+          null);
+    }
 
-		if (this.color.equals(RoomConstant.ATTACK_HIGHLIGHT) || this.color.equals(RoomConstant.MOVE_HIGHLIGHT)) {
-			g.setColor(color);
-			g.fillOval(0, 0, (int) buttonDimension.getWidth(), (int) buttonDimension.getHeight());
-		}
-		if (image != null) {
-			g.drawImage(image, 0, 0, (int) buttonDimension.getWidth(), (int) buttonDimension.getHeight(), null);
-		}
+    if (this.color.equals(RoomConstant.ATTACK_HIGHLIGHT) || this.color.equals(RoomConstant.MOVE_HIGHLIGHT)) {
+      g.setColor(color);
+      g.fillOval(0, 0, (int) buttonDimension.getWidth(), (int) buttonDimension.getHeight());
+    }
+    if (image != null) {
+      g.drawImage(image, 0, 0, (int) buttonDimension.getWidth(), (int) buttonDimension.getHeight(), null);
+    }
 
-	}
+  }
 
-	/**
-	 * @param object paints the image of the object
-	 */
-	public void drawGameObject(GameObject object) {
-		image = ImageModifier.scaleMaintainingAspectRatio(object.getTextureImage(), buttonDimension);
-		repaint();
-	}
+  /**
+   * @param object paints the image of the object
+   */
+  public void drawGameObject(final GameObject object) {
+    image = ImageModifier.scaleMaintainingAspectRatio(object.getTextureImage(), buttonDimension);
+    repaint();
+  }
 
-	public void actionPerformed(ActionEvent e) {
-		this.gameAreaController.makeAction(pos);
-	}
+  @Override
+  public void actionPerformed(final ActionEvent e) {
+    this.gameAreaController.makeAction(pos);
+  }
 
-	/**
-	 * Method to remove the sprite from a GameButton
-	 */
-	public void removeSprite() {
-		this.image = null;
-		repaint();
-	}
+  /**
+   * Method to remove the sprite from a GameButton
+   */
+  public void removeSprite() {
+    this.image = null;
+    repaint();
+  }
 
-	/**
-	 * Method that highlight the background with the passed image
-	 * 
-	 * @param image the image to place on background
-	 */
-	public void highlightCell(Color backgroundColor) {
-		this.setEnabled(true);
-		this.color = backgroundColor;
-	}
+  /**
+   * Method that highlight the background with the passed image
+   * 
+   * @param image the image to place on background
+   */
+  public void highlightCell(final Color backgroundColor) {
+    this.setEnabled(true);
+    this.color = backgroundColor;
+  }
 
-	/**
-	 * Method that remove the highlight from the cell
-	 */
-	public void removeHighlight() {
-		this.setEnabled(false);
-		this.color = RoomConstant.BASIC_CELL_COLOR;
-		repaint();
-	}
+  /**
+   * Method that remove the highlight from the cell
+   */
+  public void removeHighlight() {
+    this.setEnabled(false);
+    this.color = RoomConstant.BASIC_CELL_COLOR;
+    repaint();
+  }
 }
