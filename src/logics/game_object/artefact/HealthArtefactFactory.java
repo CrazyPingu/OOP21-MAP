cb041133c197e3f2d85fcc426521214a31fff9ea
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-05-22 15:28:30 +0200

    add final and indented

--- src/logics/game_object/artefact/HealthArtefactFactory.java
+++ src/logics/game_object/artefact/HealthArtefactFactory.java
@@ -9,20 +9,20 @@ public interface HealthArtefactFactory {
    * @param pos is the artefact spawn position
    * @return an artefact that heal the subject
    */
-  public Artefact littleHealArtefact(Pair<Integer, Integer> pos);
+  Artefact littleHealArtefact(Pair<Integer, Integer> pos);
 
   /**
    *
    * @param pos is the artefact spawn position
    * @return an artefact that heal the subject
    */
-  public Artefact bigHealArtefact(Pair<Integer, Integer> pos);
+  Artefact bigHealArtefact(Pair<Integer, Integer> pos);
 
   /**
    *
    * @param pos is the artefact spawn position
    * @return an artefact that extend the subject's life
    */
-  public Artefact extendsLifeArtefact(Pair<Integer, Integer> pos);
+  Artefact extendsLifeArtefact(Pair<Integer, Integer> pos);
 
 }
