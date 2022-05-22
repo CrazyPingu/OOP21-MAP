39531d5950f0e4800d3b1c85d92fd5f568b09e35
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-22 16:27:51 +0200

    Fix roomTest

--- src/test/RoomTest.java
+++ src/test/RoomTest.java
@@ -23,50 +23,42 @@ import utilities.texture.EntityTexture;
  *
  */
 public class RoomTest {
-	private RoomFactory roomFactory;
-	private WeaponFactory weaponFactory;
-	private MovementFactory movementFactory;
-	private Player player;
+  private WeaponFactory weaponFactory = new WeaponFactoryImpl();;
+  private MovementFactory movementFactory = new MovementFactoryImpl();;
+  private Player player = new Player(new ExtendibleMaxLifeSystem(4, 10, 20), weaponFactory.createAxe(),
+      movementFactory.stepMovement(), "Marcello", EntityTexture.PLAYER);
+  private RoomFactory roomFactory = new RoomFactoryImpl(player);;
 
-	@org.junit.Before
-	public void init() {
-		this.weaponFactory = new WeaponFactoryImpl();
-		this.movementFactory = new MovementFactoryImpl();
-		this.player = new Player(new ExtendibleMaxLifeSystem(4, 10, 20), weaponFactory.createAxe(),
-				movementFactory.stepMovement(), "Marcello", EntityTexture.PLAYER);
-		this.roomFactory = new RoomFactoryImpl(player);
-	}
+  @org.junit.Test
+  /**
+   * testing the correct dimension of the big room
+   */
+  public void bigRoomSize() {
+    final Room room = this.roomFactory.createBigRoom();
+    assertEquals(room.getSize(), new Pair<Integer, Integer>(RoomConstant.MAX_X, RoomConstant.MAX_Y));
+  }
 
-	@org.junit.Test
-	/**
-	 * testing the correct dimension of the big room
-	 */
-	public void bigRoomSize() {
-		Room room = this.roomFactory.createBigRoom();
-		assertEquals(room.getSize(), new Pair<Integer, Integer>(RoomConstant.MAX_X, RoomConstant.MAX_Y));
-	}
+  @org.junit.Test
+  /**
+   * testing the correct dimension of the small room
+   */
+  public void smallRoomSize() {
+    final Room room = this.roomFactory.createSmallRoom();
+    assertEquals(room.getSize(), new Pair<Integer, Integer>(RoomConstant.MIN_X, RoomConstant.MIN_Y));
+  }
 
-	@org.junit.Test
-	/**
-	 * testing the correct dimension of the small room
-	 */
-	public void smallRoomSize() {
-		Room room = this.roomFactory.createSmallRoom();
-		assertEquals(room.getSize(), new Pair<Integer, Integer>(RoomConstant.MIN_X, RoomConstant.MIN_Y));
-	}
-
-	@org.junit.Test
-	/**
-	 * testing the correct spawn of the enemy
-	 */
-	public void forbiddenZombieSpawn() {
-		Room room = this.roomFactory.createRandomRoom();
-		boolean flag = false;
-		for (SimpleEnemy x : room.getEnemyList()) {
-			if (x.getPos().getX() < RoomConstant.FORBIDDEN_ZOMBIE_SPAWN) {
-				flag = true;
-			}
-		}
-		assertFalse(flag);
-	}
+  @org.junit.Test
+  /**
+   * testing the correct spawn of the enemy
+   */
+  public void forbiddenZombieSpawn() {
+    final Room room = this.roomFactory.createRandomRoom();
+    boolean flag = false;
+    for (final SimpleEnemy x : room.getEnemyList()) {
+      if (x.getPos().getX() < RoomConstant.FORBIDDEN_ZOMBIE_SPAWN) {
+        flag = true;
+      }
+    }
+    assertFalse(flag);
+  }
 }
