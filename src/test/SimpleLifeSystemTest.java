02d021ba46797f8a341ecebfac18473f30fab252
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-05-22 17:26:02 +0200

    indentation and bugfix

--- src/test/SimpleLifeSystemTest.java
+++ src/test/SimpleLifeSystemTest.java
@@ -7,51 +7,54 @@ import org.junit.jupiter.api.Test;
 import logics.life.SimpleLifeSystem;
 
 public class SimpleLifeSystemTest {
-	final private int HEALTH = 9;
-	final private int DAMAGE = 1;
+  private static final int HEALTH = 9;
+  private static final int DAMAGE = 1;
 
-	@Test
-	/*
-	 * test the correct creation of a SimpleLifeSystem instantiation.
-	 */
-	public void createLifeTest() {
-		SimpleLifeSystem life = new SimpleLifeSystem(this.HEALTH);
-		assertTrue(life.getCurrentHealth() == this.HEALTH);
-	}
+  @Test
+  @SuppressWarnings("PMD.SimplifiableTestAssertion")
+  /*
+   * test the correct creation of a SimpleLifeSystem instantiation.
+   */
+  public void createLifeTest() {
+    final SimpleLifeSystem life = new SimpleLifeSystem(HEALTH);
+    assertTrue(life.getCurrentHealth() == HEALTH);
+  }
 
-	@Test
-	/*
-	 * test the proper operation of the method damage.
-	 */
-	public void damageTest() {
-		SimpleLifeSystem life = new SimpleLifeSystem(this.HEALTH);
-		life.damage(-this.DAMAGE); 		//test that damage don't work with negative value
-		assertTrue(life.getCurrentHealth() == this.HEALTH);
-		life.damage(this.DAMAGE);
-		assertTrue(life.getCurrentHealth() == this.HEALTH - this.DAMAGE);
-		life.damage(this.DAMAGE);
-		assertTrue(life.getCurrentHealth() == this.HEALTH - (2*this.DAMAGE));
-	}
+  @Test
+  @SuppressWarnings("PMD.SimplifiableTestAssertion")
+  /*
+   * test the proper operation of the method damage.
+   */
+  public void damageTest() {
+    final SimpleLifeSystem life = new SimpleLifeSystem(HEALTH);
+    life.damage(-DAMAGE); // test that damage don't work with negative value
+    assertTrue(life.getCurrentHealth() == HEALTH);
+    life.damage(DAMAGE);
+    assertTrue(life.getCurrentHealth() == HEALTH - DAMAGE);
+    life.damage(DAMAGE);
+    assertTrue(life.getCurrentHealth() == HEALTH - (2 * DAMAGE));
+  }
 
-	@Test
-	/*
-	 * test that the health value does not go below zero
-	 */
-	public void healthNotBelowZeroTest() {
-		SimpleLifeSystem life = new SimpleLifeSystem(this.HEALTH);
-		life.damage(life.getCurrentHealth() + this.DAMAGE);
-		assertTrue(life.getCurrentHealth() == 0);
-	}
-	
-	@Test
-	/*
-	 * test the correct working of the isDead method 
-	 */
-	public void isDeadTest() { 
-		SimpleLifeSystem life = new SimpleLifeSystem(this.HEALTH);
-		assertFalse(life.isDead());
-		life.damage(life.getCurrentHealth() + this.DAMAGE);
-		assertTrue(life.isDead());
-		
-	}
+  @Test
+  @SuppressWarnings("PMD.SimplifiableTestAssertion")
+  /*
+   * test that the health value does not go below zero
+   */
+  public void healthNotBelowZeroTest() {
+    final SimpleLifeSystem life = new SimpleLifeSystem(HEALTH);
+    life.damage(life.getCurrentHealth() + DAMAGE);
+    assertTrue(life.getCurrentHealth() == 0);
+  }
+
+  @Test
+  /*
+   * test the correct working of the isDead method
+   */
+  public void isDeadTest() {
+    final SimpleLifeSystem life = new SimpleLifeSystem(HEALTH);
+    assertFalse(life.isDead());
+    life.damage(life.getCurrentHealth() + DAMAGE);
+    assertTrue(life.isDead());
+
+  }
 }
