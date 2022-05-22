762930f8ca6b738d9f6c24374f6653e912c885b3
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-22 15:47:03 +0200

    Add final to fields

--- src/logics/game_object/entity/enemy/EnemyFactory.java
+++ src/logics/game_object/entity/enemy/EnemyFactory.java
@@ -9,31 +9,25 @@ import utilities.Pair;
  *
  */
 public interface EnemyFactory {
-
-	/**
-	 * @return a new enemy using a stick as a weapon.
-	 */
-	public SimpleEnemy createZombieStick(Pair<Integer, Integer> pos);
-	
-	/**
-	 * @return a new enemy using an axe as a weapon.
-	 */
-	public SimpleEnemy createZombieAxe(Pair<Integer, Integer> pos);
-	
-	/**
-	 * @return a new enemy using a dagger as a weapon.
-	 */
-	public SimpleEnemy createZombieDagger(Pair<Integer, Integer> pos);
-	
-	/**
-	 * @return a new enemy using a tube as a weapon.
-	 */
-	public SimpleEnemy createZombieTube(Pair<Integer, Integer> pos);
-	
-	/**
-	 * @return a new enemy using a gun as a weapon.
-	 */
-	public SimpleEnemy createZombieGun(Pair<Integer, Integer> pos);
-
+    /**
+     * @return a new enemy using a stick as a weapon.
+     */
+    SimpleEnemy createZombieStick(Pair<Integer, Integer> pos);
+    /**
+     * @return a new enemy using an axe as a weapon.
+     */
+    SimpleEnemy createZombieAxe(Pair<Integer, Integer> pos);
+    /**
+     * @return a new enemy using a dagger as a weapon.
+     */
+    SimpleEnemy createZombieDagger(Pair<Integer, Integer> pos);
+    /**
+     * @return a new enemy using a tube as a weapon.
+     */
+    SimpleEnemy createZombieTube(Pair<Integer, Integer> pos);
+    /**
+     * @return a new enemy using a gun as a weapon.
+     */
+    SimpleEnemy createZombieGun(Pair<Integer, Integer> pos);
 
 }
