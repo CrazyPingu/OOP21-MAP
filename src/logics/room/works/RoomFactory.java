8f6ec327920b56210a3ed63cb8c2cff97e8994b8
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-05-22 15:05:30 +0200

    Fix RoomFactory interface

--- src/logics/room/works/RoomFactory.java
+++ src/logics/room/works/RoomFactory.java
@@ -10,16 +10,16 @@ public interface RoomFactory {
 	/**
 	 * @return a new room that will be the maximum size.
 	 */
-	public Room createBigRoom();
+	Room createBigRoom();
 	
 	/**
 	 * @return a new room that will be the minimum size.
 	 */
-	public Room createSmallRoom();
+	Room createSmallRoom();
 	
 	/**
 	 * @return a new room that will have a random size.
 	 */
-	public Room createRandomRoom();
+	Room createRandomRoom();
 	
 }
