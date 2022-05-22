4ef34f3b74192612000ea526ced44b0858c18763
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-05-22 15:14:08 +0200

    add final

--- src/logics/game_object/artefact/ActionNumberArtefactFactoryImpl.java
+++ src/logics/game_object/artefact/ActionNumberArtefactFactoryImpl.java
@@ -7,11 +7,11 @@ import utilities.texture.ArtefactTexture;
 public class ActionNumberArtefactFactoryImpl implements ActionNumberArtefactFactory {
 
 	@Override
-	public Artefact increaseActionNumberArtefact(Pair<Integer, Integer> pos) {
+	public Artefact increaseActionNumberArtefact(final Pair<Integer, Integer> pos) {
 		return new Artefact(pos, "number action increaser", ArtefactTexture.ACTION_NUMBER_INCREASER) {
 
 			@Override
-			public void execute(ArtefactUserObject subject) {
+			public void execute(final ArtefactUserObject subject) {
 				subject.increaseActionNumber();
 			}
 		};
