2dddde6776d481eff2006267547ee068c1bf89eb
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-05-24 19:40:44 +0200

    modified javadoc

--- src/logics/life/HealSystem.java
+++ src/logics/life/HealSystem.java
@@ -1,18 +1,24 @@
 package logics.life;
 
-public interface HealSystem {
-	
-	/**
-	 * 
-	 * @param healValue represent the amount of life to increase. The helth value
-	 *                  can't be greater than max health. Don't work with negative
-	 *                  values
-	 */
-	void heal(int healValue); 
+/**
+ * 
+ * this interface declare the methods for a life system that can use a method to
+ * heal
+ *
+ */
+public interface HealSystem extends LifeSystem {
 
-	/**
-	 * 
-	 * @return the maximum amount of health that can be reached
-	 */
-	int getMaxHealth(); 
+  /**
+   * 
+   * @param healValue represent the amount of life to increase. The helth value
+   *                  can't be greater than max health. Don't work with negative
+   *                  values
+   */
+  void heal(int healValue);
+
+  /**
+   * 
+   * @return the maximum amount of health that can be reached
+   */
+  int getMaxHealth();
 }
