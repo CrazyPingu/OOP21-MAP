a25327dd3aa864708997f027c6b0b94747ad5662
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-05-31 11:22:52 +0200

    movement refactor

--- src/controller/enemy_ai/EnemyAIImpl.java
+++ src/controller/enemy_ai/EnemyAIImpl.java
@@ -1,7 +1,5 @@
 package controller.enemy_ai;
 
-import java.util.List;
-
 import controller.GameLoop;
 import model.game_object.entity.Player;
 import model.game_object.entity.SimpleEnemy;
@@ -15,36 +13,37 @@ import utilities.RoomConstant;
  */
 public class EnemyAIImpl implements EnemyAI {
 
-    private final GameLoop loop;
+	private final GameLoop loop;
 
-    public EnemyAIImpl(final GameLoop loop) {
-        this.loop = loop;
-    }
+	public EnemyAIImpl(final GameLoop loop) {
+		this.loop = loop;
+	}
 
-    @Override
-    public Pair<Integer, Integer> move(final SimpleEnemy enemy) {
-        final List<Pair<Integer, Integer>> enemyReachableArea = enemy.getMovementSystem().reachableCells(enemy.getPos(),
-                this.loop.getRoom().getSize());
-        final Player player = this.loop.getRoom().getPlayer();
-        Pair<Integer, Integer> newEnemyPos = enemy.getPos();
-        int minDistance = Integer.MAX_VALUE;
-        int distance;
-        for (final Pair<Integer, Integer> cell : enemyReachableArea) {
-            distance = Math.abs(cell.getX() - player.getPos().getX()) + Math.abs(cell.getY() - player.getPos().getY());
-            if (distance < minDistance
-                    && !(RoomConstant.cellsOccupated(this.loop.getRoom().getEnemyList(),
-                            this.loop.getRoom().getArtefactList(), player, cell))) {
-                newEnemyPos = cell;
-                minDistance = distance;
-            }
-        }
-        return newEnemyPos;
-    }
+	@Override
+	public Pair<Integer, Integer> move(final SimpleEnemy enemy) {
+		final Player player = this.loop.getRoom().getPlayer();
+		return enemy.getMovementSystem().reachableCells(enemy.getPos(), this.loop.getRoom().getSize()).stream()
+				.sorted((p1, p2) -> Integer.compare(calculateDistanceFromPlayer(p1, player.getPos()),
+						calculateDistanceFromPlayer(p2, player.getPos())))
+				.filter(i -> !RoomConstant.cellsOccupated(this.loop.getRoom().getEnemyList(),
+						this.loop.getRoom().getArtefactList(), player, i))
+				.findFirst().get();
+	}
 
-    @Override
-    public boolean isPlayerInAttackArea(final SimpleEnemy enemy, final Player player, final Pair<Integer, Integer> roomSize) {
-        return enemy.getWeapon().getAttackArea(enemy.getPos(), roomSize).stream()
+	@Override
+	public boolean isPlayerInAttackArea(final SimpleEnemy enemy, final Player player,
+			final Pair<Integer, Integer> roomSize) {
+		return enemy.getWeapon().getAttackArea(enemy.getPos(), roomSize).stream()
 				.anyMatch(i -> i.equals(player.getPos()));
-    }
+	}
+
+	/**
+	 * @param cell from which is calculated the distance
+	 * @param playerPos player's current position
+	 * @return the distance between player's current position and given cell
+	 */
+	private int calculateDistanceFromPlayer(final Pair<Integer, Integer> cell, final Pair<Integer, Integer> playerPos) {
+		return Math.abs(cell.getX() - playerPos.getX()) + Math.abs(cell.getY() - playerPos.getY());
+	}
 
 }
