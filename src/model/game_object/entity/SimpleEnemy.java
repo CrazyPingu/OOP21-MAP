e4dd8d90190d106e6981d55b12f6a4839331b80f
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-06-03 09:37:14 +0200

    now this class wraps the class entity

--- src/model/game_object/entity/SimpleEnemy.java
+++ src/model/game_object/entity/SimpleEnemy.java
@@ -15,87 +15,25 @@ import utilities.Pair;
 
 /**
  * 
- * Implements all the common method for a simple game object able to hold a
- * weapon, move and take damage. This class is used for the enemy
- * implementation.
+ * Wraps the entity class and allow a simple way to distingue a entity from a
+ * simpleEnemy
  *
  */
-public class SimpleEnemy implements MovingObject, WeaponizedObject, KillableObject, GameObject {
-
-  private final LifeSystem life;
-  private final Weapon weapon;
-  private final Movement movement;
-  private final String name;
-  private final Image textureImage;
-  private Pair<Integer, Integer> pos;
+public class SimpleEnemy extends Entity {
 
   /**
    * 
-   * @param life         is the life system of the entity
-   * @param pos          is the position of the entity
-   * @param weapon       is the weapon the entity is holding
-   * @param movement     is the movement sistem of the entity
-   * @param name         the name of the entity
-   * @param textureImage the texture of the entity. Texture can be found in
+   * @param life         is the life system of the SimpleEnemy
+   * @param pos          is the position of the SimpleEnemy
+   * @param weapon       is the weapon the SimpleEnemy is holding
+   * @param movement     is the movement sistem of the SimpleEnemy
+   * @param name         the name of the SimpleEnemy
+   * @param textureImage the texture of the SimpleEnemy. Texture can be found in
    *                     utilis.texture
    */
   public SimpleEnemy(final LifeSystem life, final Pair<Integer, Integer> pos, final Weapon weapon,
       final Movement movement, final String name, final Image textureImage) {
-    this.life = life;
-    this.pos = pos;
-    this.weapon = weapon;
-    this.movement = movement;
-    this.name = name;
-    this.textureImage = textureImage;
-  }
-
-  @Override
-  public void damage(final int damageValue) {
-    this.life.damage(damageValue);
-  }
-
-  @Override
-  public int getHealth() {
-    return life.getCurrentHealth();
-  }
-
-  @Override
-  public Boolean isDead() {
-    return this.life.isDead();
-  }
-
-  @Override
-  public void setPos(final Pair<Integer, Integer> pos) {
-    this.pos = pos;
-  }
-
-  public Pair<Integer, Integer> getPos() {
-    return this.pos;
-  }
-
-  @Override
-  public Weapon getWeapon() {
-    return this.weapon;
-  }
-
-  @Override
-  public Optional<List<Pair<Integer, Integer>>> getReachableArea(final Pair<Integer, Integer> size) {
-    return Optional.of(movement.reachableCells(pos, size));
-  }
-
-  @Override
-  public String getName() {
-    return this.name;
-  }
-
-  @Override
-  public Image getTextureImage() {
-    return this.textureImage;
-  }
-
-  public String toString() {
-    return "name = " + name + " " + "health = " + this.life.getCurrentHealth() + " " + "weapon damage = "
-        + this.weapon.getDamage();
+    super(life, pos, weapon, movement, name, textureImage);
   }
 
 }
