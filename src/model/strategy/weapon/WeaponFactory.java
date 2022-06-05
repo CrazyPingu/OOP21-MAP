5a92af6e1294c87907e1a7b8c43345771a2c654f
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-06-05 14:34:39 +0200

    Add enum for weapons names and damage values

--- src/model/strategy/weapon/WeaponFactory.java
+++ src/model/strategy/weapon/WeaponFactory.java
@@ -6,6 +6,43 @@ package model.strategy.weapon;
  *
  */
 public interface WeaponFactory {
+    
+    enum Damage {
+        ONE(1),
+        TWO(2),
+        THREE(3),
+        FOUR(4);
+        
+        private final int damage;
+
+        Damage(final int damage) {
+            this.damage = damage;
+        }
+
+        public int getDamage() {
+            return this.damage;
+        }
+
+    }
+
+    enum Name {
+        STICK("stick"),
+        AXE("axe"),
+        DAGGER("dagger"),
+        TUBE("tube"),
+        GUN("gun");
+
+        private String name;
+
+        Name(final String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }    
+    
     /**
      * @return a stick
      */
