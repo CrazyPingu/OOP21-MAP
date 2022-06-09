a9dcd0261c469989ef241326a52a929363dbb2eb
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-06-09 21:34:47 +0200

    fixed test

--- src/test/CrossAreaTest.java
+++ src/test/CrossAreaTest.java
@@ -1,55 +1,98 @@
 package test;
 
-import org.junit.jupiter.api.Test;
-
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 import model.strategy.Strategy;
 import model.strategy.concrete_strategies.CrossArea;
 import model.strategy.concrete_strategies.VariableDistanceConstants;
 import utilities.Pair;
 
 class CrossAreaTest {
-  private final Pair<Integer, Integer> size = new Pair<>(7, 6);
-  private Pair<Integer, Integer> pos;
-  private Strategy strategy = new CrossArea(VariableDistanceConstants.DOUBLE_DISTANCE);
+  private Pair<Integer, Integer> size, pos;
+  final private Strategy crossArea = new CrossArea(VariableDistanceConstants.SINGLE_DISTANCE);
+  final private List<Pair<Integer, Integer>> expectedResults = new ArrayList<>();
 
-  @Test
-  /*
-   * test the correct result of the algorithm when is chosen a cell in the center
-   * of the grid.
-   */
-  void innerAreaTest() {
-    System.out.println("In area Test:");
-    this.pos = new Pair<>(3, 2);
-    System.out.println(strategy.execute(pos, size));
+  @org.junit.Before
+  public void init() {
+    this.size = new Pair<>(3, 3);
+    this.expectedResults.clear();
   }
 
-  @Test
-  @SuppressWarnings("PMD.AvoidDuplicateLiterals")
-  /*
-   * test the correct result of the algorithm when is chosen a cell in the corner
-   * of the grid.
+  @org.junit.Test
+  /**
+   * test of inner area cell choice
    */
-  void cornerAreaTest() {
-    System.out.println("Corner Test:");
+  public void innerGridTest() {
+    this.pos = new Pair<>(1, 1);
+    System.out.println(crossArea.execute(this.pos, this.size));
+    expectedResults.add(new Pair<>(0, 0));
+    expectedResults.add(new Pair<>(0, 1));
+    expectedResults.add(new Pair<>(0, 2));
+    expectedResults.add(new Pair<>(1, 0));
+    expectedResults.add(new Pair<>(1, 2));
+    expectedResults.add(new Pair<>(2, 0));
+    expectedResults.add(new Pair<>(2, 1));
+    expectedResults.add(new Pair<>(2, 2));
+
+    assertEquals(expectedResults, crossArea.execute(this.pos, this.size));
+  }
+
+  @org.junit.Test
+  /**
+   * test of grid's upper left corner cell choice
+   */
+  public void upperLeftCornerTest() {
     this.pos = new Pair<>(0, 0);
-    System.out.println("pos = " + pos);
-    System.out.println("res =");
-    System.out.println(strategy.execute(pos, size));
+    System.out.println(crossArea.execute(this.pos, this.size));
+    this.expectedResults.add(new Pair<>(0, 1));
+    this.expectedResults.add(new Pair<>(1, 0));
+    this.expectedResults.add(new Pair<>(1, 1));
 
-    this.pos = new Pair<>(this.size.getX() - 1, 0);
-    System.out.println("pos = " + pos);
-    System.out.println("res =");
-    System.out.println(strategy.execute(pos, size));
+    assertEquals(expectedResults, crossArea.execute(this.pos, this.size));
+  }
 
-    this.pos = new Pair<>(0, this.size.getY() - 1);
-    System.out.println("pos = " + pos);
-    System.out.println("res =");
-    System.out.println(strategy.execute(pos, size));
+  @org.junit.Test
+  /**
+   * test of grid's bottom left corner cell choice
+   */
+  public void bottomLeftCornerTest() {
+    this.pos = new Pair<>(0, 2);
+    System.out.println(crossArea.execute(this.pos, this.size));
+    this.expectedResults.add(new Pair<>(0,1));
+    this.expectedResults.add(new Pair<>(1,1));
+    this.expectedResults.add(new Pair<>(1,2));
+    
+    assertEquals(expectedResults, crossArea.execute(this.pos, this.size));
+  }
 
-    this.pos = new Pair<>(this.size.getX() - 1, this.size.getY() - 1);
-    System.out.println("pos = " + pos);
-    System.out.println("res =");
-    System.out.println(strategy.execute(pos, size));
+  @org.junit.Test
+  /**
+   * test of grid's upper left corner cell choice
+   */
+  public void upperRightCornerTest() {
+    this.pos = new Pair<>(2, 0);
+    System.out.println(crossArea.execute(this.pos, this.size));
+    this.expectedResults.add(new Pair<>(1,0));
+    this.expectedResults.add(new Pair<>(1,1));
+    this.expectedResults.add(new Pair<>(2,1));
+
+    assertEquals(expectedResults, crossArea.execute(this.pos, this.size));
+  }
+
+  @org.junit.Test
+  /**
+   * test of grid's upper left corner cell choice
+   */
+  public void bottomRightCornerTest() 
+  {
+    this.pos = new Pair<>(2, 2);
+    System.out.println(crossArea.execute(this.pos, this.size));
+    this.expectedResults.add(new Pair<>(1,1));
+    this.expectedResults.add(new Pair<>(1,2));
+    this.expectedResults.add(new Pair<>(2,1));
+
+    assertEquals(expectedResults, crossArea.execute(this.pos, this.size));
   }
 
 }
