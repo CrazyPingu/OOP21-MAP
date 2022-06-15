25fce91a614a61b1f2139544c6fe46ffed6d2919
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-06-15 09:43:41 +0200

    Add javadoc to ostacle

--- src/model/game_object/obstacle/ObstacleFactoryImpl.java
+++ src/model/game_object/obstacle/ObstacleFactoryImpl.java
@@ -3,6 +3,11 @@ package model.game_object.obstacle;
 import utilities.Pair;
 import utilities.texture.EntityTexture;
 
+/*
+ * 
+ * Factory to create a new Obstacle 
+ *
+ */
 public class ObstacleFactoryImpl implements ObstacleFactory{
 
   public Obstacle createRock(final Pair<Integer, Integer> pos) {
