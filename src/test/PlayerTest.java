c9d60d8a8aef860ec4b3aacf4547381b1f734c47
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-06-21 15:51:09 +0200

    changed wrongs assertTrue into assertEquals and documentation fix

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -2,10 +2,9 @@ package test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import java.awt.Image;
-
 import org.junit.jupiter.api.Test;
-
 import model.game_object.entity.Player;
 import model.life_system.impl.ExtendibleMaxLifeSystem;
 import model.movement.MovementFactory;
@@ -15,6 +14,10 @@ import model.weapon.WeaponFactoryImpl;
 import utilities.Pair;
 import utilities.texture.EntityTexture;
 
+/**
+ * 
+ * Test the player class
+ */
 class PlayerTest {
   private static final int HEALTH = 9;
   private static final int MAX_HEALTH = 15;
@@ -30,57 +33,57 @@ class PlayerTest {
   private ExtendibleMaxLifeSystem life;
   private Player player;
 
-  @Test
-  /*
-   * test the correct creation of a Player instantiation.
+  /**
+   * Test the correct creation of a Player instantiation.
    */
+  @Test
   void simplePlayerCreationTest() {
     this.life = new ExtendibleMaxLifeSystem(HEALTH, MAX_HEALTH, MAX_HEALTH_LIMIT);
     this.player = new Player(life, START_POS, this.weaponFactory.createAxe(), this.movementFactory.stepMovement(), NAME,
         TEXTURE, DAMAGE);
-    assertTrue(this.player.getHealth() == HEALTH);
-    assertTrue(this.player.getMaxHealth() == MAX_HEALTH);
-    assertTrue(this.player.getPos().equals(START_POS));
-    assertTrue(NAME.equals(this.player.getName()));
+    assertEquals(this.player.getHealth(), HEALTH);
+    assertEquals(this.player.getMaxHealth(), MAX_HEALTH);
+    assertEquals(this.player.getPos(), START_POS);
+    assertEquals(NAME, this.player.getName());
     assertFalse(this.player.isDead());
 
     this.player = new Player(this.life, this.weaponFactory.createAxe(), this.movementFactory.stepMovement(), NAME,
         TEXTURE);
-    assertTrue(this.player.getHealth() == HEALTH);
-    assertTrue(this.player.getMaxHealth() == MAX_HEALTH);
-    assertTrue(this.player.getPos().equals(new Pair<Integer, Integer>(0, 0)));
-    assertTrue(NAME.equals(this.player.getName()));
+    assertEquals(this.player.getHealth(), HEALTH);
+    assertEquals(this.player.getMaxHealth(), MAX_HEALTH);
+    assertEquals(this.player.getPos(), new Pair<Integer, Integer>(0, 0));
+    assertEquals(NAME, this.player.getName());
     assertFalse(this.player.isDead());
   }
 
-  @Test
-  /*
-   * test the proper operation of the method damage.
+  /**
+   * Test the proper operation of the method damage.
    */
+  @Test
   void damageEntityTest() {
     this.life = new ExtendibleMaxLifeSystem(HEALTH, MAX_HEALTH, MAX_HEALTH_LIMIT);
     this.player = new Player(this.life, this.weaponFactory.createAxe(), this.movementFactory.stepMovement(), NAME,
         TEXTURE);
     this.player.damage(DAMAGE);
-    assertTrue(this.player.getHealth() == HEALTH - DAMAGE);
+    assertEquals(this.player.getHealth(), HEALTH - DAMAGE);
   }
 
-  @Test
-  /*
-   * test that the health value does not go below zero
+  /**
+   * Test that the health value does not go below zero
    */
+  @Test
   void healthNotBelowZeroTest() {
     this.life = new ExtendibleMaxLifeSystem(HEALTH, MAX_HEALTH, MAX_HEALTH_LIMIT);
     this.player = new Player(this.life, this.weaponFactory.createAxe(), this.movementFactory.stepMovement(), NAME,
         TEXTURE);
     this.player.damage(this.player.getHealth() + DAMAGE);
-    assertTrue(this.player.getHealth() == 0);
+    assertEquals(this.player.getHealth(), 0);
   }
 
-  @Test
-  /*
-   * test the correct functioning of the isDead method
+  /**
+   * Test the correct functioning of the isDead method
    */
+  @Test
   void isDeadTest() {
     this.life = new ExtendibleMaxLifeSystem(HEALTH, MAX_HEALTH, MAX_HEALTH_LIMIT);
     this.player = new Player(this.life, this.weaponFactory.createAxe(), this.movementFactory.stepMovement(), NAME,
@@ -89,10 +92,10 @@ class PlayerTest {
     assertTrue(this.player.isDead());
   }
 
-  @Test
-  /*
-   * test the heal method
+  /**
+   * Test the heal method
    */
+  @Test
   void healTest() {
     this.life = new ExtendibleMaxLifeSystem(HEALTH, MAX_HEALTH, MAX_HEALTH_LIMIT);
     this.player = new Player(this.life, this.weaponFactory.createAxe(), this.movementFactory.stepMovement(), NAME,
@@ -100,21 +103,21 @@ class PlayerTest {
 
     // test the correct funcioning of the method heal
     this.player.heal(HEAL);
-    assertTrue(this.player.getHealth() == HEALTH + HEAL);
+    assertEquals(this.player.getHealth(), HEALTH + HEAL);
 
     // test than health can reach the max health value
     this.player.heal(MAX_HEALTH);
-    assertTrue(this.player.getHealth() == MAX_HEALTH);
+    assertEquals(this.player.getHealth(), MAX_HEALTH);
 
     // test the health value can't be increase into a value greater than max health
     this.player.heal(MAX_HEALTH + 1);
-    assertTrue(this.player.getHealth() == MAX_HEALTH);
+    assertEquals(this.player.getHealth(), MAX_HEALTH);
   }
 
-  @Test
-  /*
-   * test that the heal method do nothing after the player is dead
+  /**
+   * Test that the heal method do nothing after the player is dead
    */
+  @Test
   void healAfterDeath() {
     this.life = new ExtendibleMaxLifeSystem(HEALTH, MAX_HEALTH, MAX_HEALTH_LIMIT);
     this.player = new Player(this.life, this.weaponFactory.createAxe(), this.movementFactory.stepMovement(), NAME,
@@ -123,36 +126,36 @@ class PlayerTest {
     // the player die
     this.player.damage(this.player.getHealth());
     assertTrue(this.player.isDead());
-    assertTrue(this.player.getHealth() == 0);
+    assertEquals(this.player.getHealth(), 0);
 
     // try to heal the player
     this.player.heal(HEAL);
     assertTrue(this.player.isDead());
-    assertTrue(this.player.getHealth() == 0);
+    assertEquals(this.player.getHealth(), 0);
   }
 
-  @Test
-  /*
+  /**
    * test the max health stay in the correct range specified in the constructor
    */
+  @Test
   void maxHealthInRange() {
     this.life = new ExtendibleMaxLifeSystem(HEALTH, MAX_HEALTH, MAX_HEALTH_LIMIT);
     this.player = new Player(this.life, this.weaponFactory.createAxe(), this.movementFactory.stepMovement(), NAME,
         TEXTURE);
     // test the functioning of the method setMaxHealth
     this.player.setMaxHealth(MAX_HEALTH_LIMIT - 3);
-    assertTrue(this.player.getMaxHealth() == MAX_HEALTH_LIMIT - 3);
+    assertEquals(this.player.getMaxHealth(), MAX_HEALTH_LIMIT - 3);
     this.player.setMaxHealth(MAX_HEALTH_LIMIT);
-    assertTrue(this.player.getMaxHealth() == MAX_HEALTH_LIMIT);
+    assertEquals(this.player.getMaxHealth(), MAX_HEALTH_LIMIT);
 
     // test the max health value can't be set greater than the limit
     this.player.setMaxHealth(MAX_HEALTH_LIMIT + 5);
-    assertTrue(this.player.getMaxHealth() == MAX_HEALTH_LIMIT);
+    assertEquals(this.player.getMaxHealth(), MAX_HEALTH_LIMIT);
   }
 
   @Test
-  /*
-   * test toString result
+  /**
+   * Test toString result
    */
   void playerPrintTest() {
     this.life = new ExtendibleMaxLifeSystem(HEALTH, MAX_HEALTH, MAX_HEALTH_LIMIT);
